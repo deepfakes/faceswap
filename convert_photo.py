6f42b30fd7e4b553e2288853b255e713e1a0a4cb
Hidde Jansen <hjansen@exchange.transip.nl>
2017-12-19 23:40:29 +0100

    PEP8

--- convert_photo.py
+++ convert_photo.py
@@ -6,15 +6,15 @@ from lib.utils import get_image_paths, get_folder
 from lib.faces_detect import crop_faces
 from lib.faces_process import convert_one_image
 
-output_dir = get_folder( 'modified' )
+output_dir = get_folder('modified')
 
-images_SRC = get_image_paths( 'original' )
+images_SRC = get_image_paths('original')
 
 for fn in images_SRC:
     image = cv2.imread(fn)
-    for ((x,w),(y,h),face) in crop_faces( image ):
-        new_face = convert_one_image( cv2.resize(face, (256,256)) )
-        image[slice(y,y+h),slice(x,x+w)] = cv2.resize(new_face, (w,h))
-    
+    for ((x, w), (y, h), face) in crop_faces(image):
+        new_face = convert_one_image(cv2.resize(face, (256, 256)))
+        image[slice(y, y + h), slice(x, x + w)] = cv2.resize(new_face, (w, h))
+
     output_file = output_dir / Path(fn).name
-    cv2.imwrite( str(output_file) , image )
+    cv2.imwrite(str(output_file), image)
--- extract.py
+++ extract.py
@@ -1,17 +1,16 @@
 import cv2
-import numpy
 from pathlib import Path
 
 from lib.utils import get_image_paths, get_folder
 from lib.faces_detect import crop_faces
 
-output_dir = get_folder( 'extract' )
+output_dir = get_folder('extract')
 
-images_SRC = get_image_paths( 'src' )
+images_SRC = get_image_paths('src')
 
 for fn in images_SRC:
     image = cv2.imread(fn)
-    for (idx, (p1,p2,img)) in enumerate(crop_faces( image )):
-        final = cv2.resize(img, (256,256))
+    for (idx, (p1, p2, img)) in enumerate(crop_faces(image)):
+        final = cv2.resize(img, (256, 256))
         output_file = output_dir / Path(fn).stem
-        cv2.imwrite( str(output_file) + str(idx) + Path(fn).suffix, final )
+        cv2.imwrite(str(output_file) + str(idx) + Path(fn).suffix, final)
