f542c58a48e87878028b7639a3c0296bdb351071
Clorr <colin@clorr.fr>
2017-12-21 23:13:47 +0100

    Adding face_recognition
    Added face_recongition from
    https://github.com/ageitgey/face_recognition/
    
    It is based on dlib, but does not have the glitches I had previously!
    
    Also reworked Dockerfile (sticking to Python2 for now)

--- lib/faces_detect.py
+++ lib/faces_detect.py
@@ -1,14 +1,13 @@
-import cv2
+import face_recognition
 from DetectedFace import DetectedFace
 
-# Give right path to the xml file or put it directly in current folder
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+def crop_faces(frame):
+    face_locations = face_recognition.face_locations(frame)
+    #face_encodings = face_recognition.face_encodings(frame, face_locations)
 
-def crop_faces(image):
-    # Add : cv.EqualizeHist(image, image) ?
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-
-    for (x, y, w, h) in faces:
-        yield DetectedFace(image[y: y + h, x: x + w], x, w, y, h, None)
+    for (top, right, bottom, left) in face_locations:
+        x = left
+        y = top
+        w = right - left
+        h = bottom - top
+        yield DetectedFace(frame[y: y + h, x: x + w], x, w, y, h, None)
