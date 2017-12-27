5bc9274311344147c5206662f372d066c8bba616
yangey <chen.yang@brother-bsh.com.cn>
2017-12-27 14:52:15 +0800

    avg color adjust & smooth mask

--- lib/faces_process.py
+++ lib/faces_process.py
@@ -7,8 +7,26 @@ from .model import autoencoder_A
 from .model import autoencoder_B
 from .model import encoder, decoder_A, decoder_B
 
+def adjust_avg_color(img_old,img_new):
+    w,h,c = img_new.shape
+    for i in range(img_new.shape[-1]):
+        old_avg = img_old[:, :, i].mean()
+        new_avg = img_new[:, :, i].mean()
+        diff_int = (int)(old_avg - new_avg)
+        diff_np = numpy.zeros( (w,h) ) - diff_int
+        img_new[:, :, i] -= diff_np.astype(img_new.dtype)
+
+def smooth_mask(img_old,img_new):
+    w,h,c = img_new.shape
+    crop2 = slice(0,w)
+    mask = numpy.zeros_like(img_new)
+    mask[h//15:-h//15,w//15:-w//15,:] = 255
+    mask = cv2.GaussianBlur(mask,(15,15),10)
+    img_new[crop2,crop2] = mask/255*img_new + (1-mask/255)*img_old
 
 def convert_one_image(image, model_dir="models"):
+    use_avg_color_adjust = True
+    use_smooth_mask = True
 
     encoder.load_weights(model_dir + "/encoder.h5")
     decoder_A.load_weights(model_dir + "/decoder_A.h5")
@@ -28,11 +46,19 @@ def convert_one_image(image, model_dir="models"):
     assert image.shape == (256, 256, 3)
     crop = slice(48, 208)
     face = image[crop, crop]
+    old_face = face.copy()
+
     face = cv2.resize(face, (64, 64))
     face = numpy.expand_dims(face, 0)
     new_face = autoencoder.predict(face / 255.0)[0]
     new_face = numpy.clip(new_face * 255, 0, 255).astype(image.dtype)
     new_face = cv2.resize(new_face, (160, 160))
+
+    if use_avg_color_adjust:
+        adjust_avg_color(old_face,new_face)
+    if use_smooth_mask:
+        smooth_mask(old_face,new_face)
+
     return superpose(image, new_face, crop)
     # Aligner is not ready to use yet
     # result = aligner.align(image.copy(), new_face)
