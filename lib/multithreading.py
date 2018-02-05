ff6e41b1e1456c1563fc15a076f630a648747423
Gareth Dunstone <freedom.2.the.leetle.people@gmail.com>
2018-02-05 18:37:25 +1100

    Added process number argument to multiprocessing extractor.
    Fixed progressbar for multiprocessing.

--- lib/multithreading.py
+++ lib/multithreading.py
@@ -2,12 +2,15 @@ import multiprocessing as mp
 
 method = None
 
-def pool_process(method_to_run, data):
+def pool_process(method_to_run, data, processes=None):
     global method
+    if processes is None:
+        processes = mp.cpu_count()
     method = method_to_run
-    pool = mp.Pool()
+    pool = mp.Pool(processes=processes)
+
     for i in pool.imap_unordered(runner, data):
         yield(i)
-    
+        
 def runner(item):
     return method(item)
--- scripts/extract.py
+++ scripts/extract.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from lib.cli import DirectoryProcessor
 from lib.multithreading import pool_process
 from plugins.PluginLoader import PluginLoader
+from tqdm import tqdm
 
 class ExtractTrainingData(DirectoryProcessor):
     def create_parser(self, subparser, command, description):
@@ -21,13 +22,35 @@ class ExtractTrainingData(DirectoryProcessor):
                             choices=("hog", "cnn"), # case sensitive because this is used to load a plugin.
                             default="hog",
                             help="Detector to use. 'cnn' detects much more angles but will be much more resource intensive and may fail on large files.")
+        parser.add_argument('-j', '--processes',
+                            type=int,
+                            help="Number of processes to use.")
         return parser
 
     def process(self):
         extractor_name = "Align" # TODO Pass as argument
         self.extractor = PluginLoader.get_extractor(extractor_name)()
+        self.faces_detected = 0
+        processes = self.arguments.processes
+        if processes is None:
+            for _ in tqdm(pool_process(self.processFiles, list(self.read_directory()), processes=processes)):
+                self.faces_detected +=1
+        elif processes > 1:
+            for _ in tqdm(pool_process(self.processFiles, list(self.read_directory()), processes=processes)):
+                self.faces_detected +=1
+        else:
+            try:
+                for filename in self.read_directory():
+                    image = cv2.imread(filename)
+                    for idx, face in self.get_faces(image):
+                        resized_image = self.extractor.extract(image, face, 256)
+                        output_file = self.output_dir / Path(filename).stem
+                        cv2.imwrite(str(output_file) + str(idx) + Path(filename).suffix, resized_image)
 
-        self.faces_detected = sum(pool_process(self.processFiles, list(self.read_directory())))
+            except Exception as e:
+                print('Failed to extract from image: {}. Reason: {}'.format(filename, e))
+
+        # self.faces_detected = sum()
         # multi threading affects the faces_detected count, so we update it here
 
     def processFiles(self, filename):
@@ -39,7 +62,6 @@ class ExtractTrainingData(DirectoryProcessor):
                 resized_image = self.extractor.extract(image, face, 256)
                 output_file = self.output_dir / Path(filename).stem
                 cv2.imwrite(str(output_file) + str(idx) + Path(filename).suffix, resized_image)
-            
             return count + 1
         except Exception as e:
             print('Failed to extract from image: {}. Reason: {}'.format(filename, e))
