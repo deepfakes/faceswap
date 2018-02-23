80288a56bd013ccb394f40e3ca50d90e19623598
Othniel Cundangan <othniel.cundangan@gmail.com>
2018-02-22 19:36:31 -0500

    Revert "Fix decoder output"
    This reverts commit 3a8ecb8957fe65e66282197455d775eb88455a77.

--- plugins/Model_GAN/Model.py
+++ plugins/Model_GAN/Model.py
@@ -107,7 +107,8 @@ class GANModel():
             #x = Conv2D(4, kernel_size=5, padding='same')(x)   
             alpha = Conv2D(1, kernel_size=5, padding='same', activation="sigmoid")(x)
             rgb = Conv2D(3, kernel_size=5, padding='same', activation="tanh")(x)
-            return Model(input_, [alpha, rgb])
+            out = concatenate([alpha, rgb])
+            return Model(input_, out )
         
         encoder = Encoder()
         decoder_A = Decoder_ps()
