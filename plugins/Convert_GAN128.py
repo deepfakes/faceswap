cec05db8d1670a7ecd1c3ece147253aaacd3f1a9
Othniel Cundangan <othniel.cundangan@gmail.com>
2018-02-24 11:45:52 -0500

    Add gan128 conversion

---
+++ plugins/Convert_GAN128.py
@@ -0,0 +1,20 @@
+# Based on the https://github.com/shaoanlu/faceswap-GAN repo (master/FaceSwap_GAN_v2_train.ipynb)
+
+import cv2
+import numpy
+
+class Convert(object):
+    def __init__(self, encoder, **kwargs):
+        self.encoder = encoder
+
+    def patch_image( self, original, face_detected ):
+        face = cv2.resize(face_detected.image, (128, 128))
+        face = numpy.expand_dims(face, 0) / 255.0 * 2 - 1
+        fake_output = self.encoder(face)
+        mask = fake_output[0][:,:,:, :1]
+        new_face = fake_output[0][:,:,:, 1:]
+        new_face = mask * new_face + (1 - mask) * face
+        new_face = numpy.clip((new_face[0] + 1) * 255 / 2, 0, 255).astype('uint8')
+
+        original[face_detected.y: face_detected.y + face_detected.h, face_detected.x: face_detected.x + face_detected.w] = cv2.resize(new_face, (face_detected.w, face_detected.h))
+        return original
