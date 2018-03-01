232d9313afabc075e28c1a42047b22e0568200e6
iperov <lepersorium@gmail.com>
2018-03-01 14:32:11 +0400

    port 'face_alignment' from PyTorch to Keras. (#228)
    * port 'face_alignment' from PyTorch to Keras. It works x2 faster, but initialization takes 20secs.
    
    2DFAN-4.h5 and mmod_human_face_detector.dat included in lib\FaceLandmarksExtractor
    
    fixed dlib vs tensorflow conflict: dlib must do op first, then load keras model, otherwise CUDA OOM error
    
    if face location not found by CNN, its try to find by HOG.
    
    removed this:
    -        if face.landmarks == None:
    -            print("Warning! landmarks not found. Switching to crop!")
    -            return cv2.resize(face.image, (size, size))
    because DetectedFace always has landmarks
    
    * removing DetectedFace.landmarks

--- lib/faces_detect.py
+++ lib/faces_detect.py
@@ -1,38 +1,19 @@
-import dlib
-import face_recognition
-import face_recognition_models
+from lib import FaceLandmarksExtractor
 
 def detect_faces(frame, model="hog"):
-    face_locations = face_recognition.face_locations(frame, model=model)
-    landmarks = _raw_face_landmarks(frame, face_locations)
-
-    for ((y, right, bottom, x), landmarks) in zip(face_locations, landmarks):
-        yield DetectedFace(frame[y: bottom, x: right], x, right - x, y, bottom - y, landmarks)
-
-# Copy/Paste (mostly) from private method in face_recognition
-predictor_68_point_model = face_recognition_models.pose_predictor_model_location()
-pose_predictor = dlib.shape_predictor(predictor_68_point_model)
-
-def _raw_face_landmarks(face_image, face_locations):
-    face_locations = [_css_to_rect(face_location) for face_location in face_locations]
-    return [pose_predictor(face_image, face_location) for face_location in face_locations]
-
-def _css_to_rect(css):
-    return dlib.rectangle(css[3], css[0], css[1], css[2])
-# end of Copy/Paste
+    fd = FaceLandmarksExtractor.extract (frame, True if model == "cnn" else False )
+    for face in fd:
+        x, y, right, bottom, landmarks = face[0][0], face[0][1], face[0][2], face[0][3], face[1]
+        yield DetectedFace(frame[y: bottom, x: right], x, right - x, y, bottom - y, landmarksXY=landmarks)
 
 class DetectedFace(object):
-    def __init__(self, image=None, x=None, w=None, y=None, h=None, landmarks=None, landmarksXY=None):
+    def __init__(self, image=None, x=None, w=None, y=None, h=None, landmarksXY=None):
         self.image = image
         self.x = x
         self.w = w
         self.y = y
         self.h = h
-        self.landmarks = landmarks
         self.landmarksXY = landmarksXY
 
     def landmarksAsXY(self):
-        if self.landmarksXY:
-            return self.landmarksXY
-        self.landmarksXY = [(p.x, p.y) for p in self.landmarks.parts()]
         return self.landmarksXY
