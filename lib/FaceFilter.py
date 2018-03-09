1e0bc0d4ef8bd378634409e90839caedd2aac22d
Lasse Hyyrynen <lasse@hyyrynen.net>
2018-03-09 16:07:27 +0200

    Clean up outputting

--- lib/FaceFilter.py
+++ lib/FaceFilter.py
@@ -10,36 +10,43 @@ class FaceFilter():
     def __init__(self, reference_file_paths, nreference_file_paths, threshold = 0.6):
         images = list(map(face_recognition.load_image_file, reference_file_paths))
         nimages = list(map(face_recognition.load_image_file, nreference_file_paths))
-        self.encodings = list(map(lambda im: face_recognition.face_encodings(im)[0], images)) # Note: we take only first face, so the reference file should only contain one face.
-        self.nencodings = list(map(lambda im: face_recognition.face_encodings(im)[0], nimages)) # Note: we take only first face, so the reference file should only contain one face.
+        # Note: we take only first face, so the reference file should only contain one face.
+        self.encodings = list(map(lambda im: face_recognition.face_encodings(im)[0], images))
+        self.nencodings = list(map(lambda im: face_recognition.face_encodings(im)[0], nimages))
         self.threshold = threshold
     
     def check(self, detected_face):
-        encodings = face_recognition.face_encodings(detected_face.image) # we could use detected landmarks, but I did not manage to do so. TODO The copy/paste below should help
+        # we could use detected landmarks, but I did not manage to do so. TODO The copy/paste below should help
+        encodings = face_recognition.face_encodings(detected_face.image)
         if encodings is not None and len(encodings) > 0:
-            distances = face_recognition.face_distance(self.encodings, encodings[0])
-            ndistances = face_recognition.face_distance(self.nencodings, encodings[0])
-            ndistance = avg(ndistances)
+            distances = list(face_recognition.face_distance(self.encodings, encodings[0]))
             distance = avg(distances)
-            nmindistance = min(ndistances)
             mindistance = min(distances)
-            nmaxdistance = max(ndistances)
             maxdistance = max(distances)
-            #distance = max(face_recognition.face_distance(self.encodings, encoding[0]))
-            if distance <= self.threshold:
-                print("Distance below threshold: %f < %f" % (distance, self.threshold))
-            chosen = distance <= self.threshold
-            if len(ndistances) > 0:
-              chosen = chosen and (mindistance < nmindistance)
-              chosen = chosen and (distance < ndistance)
+            if distance > self.threshold:
+                print("Distance above threshold: %f < %f" % (distance, self.threshold))
+                return False
+            if len(self.nencodings) > 0:
+              ndistances = list(face_recognition.face_distance(self.nencodings, encodings[0]))
+              ndistance = avg(ndistances)
+              nmindistance = min(ndistances)
+              nmaxdistance = max(ndistances)
+              if (mindistance > nmindistance):
+                  print("Distance to negative sample is smaller")
+                  return False
+              if (distance > ndistance):
+                  print("Average distance to negative sample is smaller")
+                  return False
               # k-nn classifier
-              K=min(4, int((len(distances) + len(ndistances))/2))
+              K=min(5, min(len(distances), len(ndistances)) + 1)
               N=sum(list(map(lambda x: x[0],
                     list(sorted([(1,d) for d in distances] + [(0,d) for d in ndistances],
                                 key=lambda x: x[1]))[:K])))
               ratio = N/K
-              chosen = chosen and (ratio > 0.5)
-            return chosen
+              if (ratio < 0.5):
+                  print("K-nn is %.2f" % ratio)
+                  return False
+            return True
         else:
             print("No face encodings found")
             return False
