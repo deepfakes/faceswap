cb9ff1973cae9ac2e7262bf37dda6f81573eea02
torzdf <juhudatrop@2anom.com>
2018-03-10 02:38:30 +0000

    Minor fix

--- scripts/convert.py
+++ scripts/convert.py
@@ -214,9 +214,11 @@ class ConvertImage(DirectoryProcessor):
                     if face.r != 0:
                         image = rotate_image(image, face.r)
                         image = converter.patch_image(image, face, 64 if "128" not in self.arguments.trainer else 128)
+                        # TODO: This switch between 64 and 128 is a hack for now. We should have a separate cli option for size
                         image = rotate_image(image, face.r * -1)
                     else:
                         image = converter.patch_image(image, face, 64 if "128" not in self.arguments.trainer else 128)
+                        # TODO: This switch between 64 and 128 is a hack for now. We should have a separate cli option for size
             output_file = get_folder(self.output_dir) / Path(filename).name
             cv2.imwrite(str(output_file), image)
         except Exception as e:
