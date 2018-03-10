c38de7053b1be8ceb0bd8263a3a1d1676bf5aa9f
Clorr <github@clorr.fr>
2018-03-10 10:04:18 +0100

    Add DSSIMobjective + res_block

--- plugins/Model_OriginalRetrain/Model.py
+++ plugins/Model_OriginalRetrain/Model.py
@@ -1,16 +1,20 @@
 # Based on the original https://www.reddit.com/r/deepfakes/ code sample + contribs
 
 from keras.models import Model as KerasModel
-from keras.layers import Input, Dense, Flatten, Reshape
+from keras.layers import Input, Dense, Flatten, Reshape, add
 from keras.layers.advanced_activations import LeakyReLU
 from keras.layers.convolutional import Conv2D
 from keras.optimizers import Adam
+from keras.initializers import RandomNormal
+from keras_contrib.losses import DSSIMObjective
 
 from .AutoEncoder import AutoEncoder
 from lib.PixelShuffler import PixelShuffler
 
 IMAGE_SHAPE = (64, 64, 3)
 ENCODER_DIM = 1024
+
+conv_init = RandomNormal(0, 0.02)
 
 class Model(AutoEncoder):
     def initModel(self):
@@ -21,7 +25,8 @@ class Model(AutoEncoder):
         encoder.trainable = False
         self.autoencoder_B = KerasModel(x, self.decoder_B(encoder(x)))
 
-        self.autoencoder_B.compile(optimizer=optimizer, loss='mean_absolute_error')
+        #self.autoencoder_B.compile(optimizer=optimizer, loss='mean_absolute_error')
+        self.autoencoder_B.compile(optimizer=optimizer, loss=DSSIMObjective(kernel_size=3))
 
     def converter(self, swap):
         autoencoder = self.autoencoder_B
@@ -41,6 +46,15 @@ class Model(AutoEncoder):
             x = PixelShuffler()(x)
             return x
         return block
+    
+    def res_block(self, input_tensor, f):
+        x = input_tensor
+        x = Conv2D(f, kernel_size=3, kernel_initializer=conv_init, use_bias=False, padding="same")(x)
+        x = LeakyReLU(alpha=0.2)(x)
+        x = Conv2D(f, kernel_size=3, kernel_initializer=conv_init, use_bias=False, padding="same")(x)
+        x = add([x, input_tensor])
+        x = LeakyReLU(alpha=0.2)(x)
+        return x
 
     def Encoder(self):
         input_ = Input(shape=IMAGE_SHAPE)
@@ -63,5 +77,7 @@ class Model(AutoEncoder):
         x = self.upscale(64)(x)
         x = self.upscale(32)(x)
         x = self.upscale(16)(x)
+        x = self.res_block(x, 16)
+        x = self.res_block(x, 16)
         x = Conv2D(3, kernel_size=5, padding='same', activation='sigmoid')(x)
         return KerasModel(input_, x)
