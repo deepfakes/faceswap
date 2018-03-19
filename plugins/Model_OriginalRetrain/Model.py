0b5eb0aa1421a6d76eeff28f5ddccc65b6fa4fc4
Clorr <github@clorr.fr>
2018-03-19 11:07:02 +0100

    Trying Conv2DTranspose

--- plugins/Model_OriginalRetrain/Model.py
+++ plugins/Model_OriginalRetrain/Model.py
@@ -1,10 +1,9 @@
 # Based on the original https://www.reddit.com/r/deepfakes/ code sample + contribs
 
 from keras.models import Model as KerasModel
-from keras.layers import Input, Dense, Flatten, Reshape, Conv2DTranspose, Activation
-from keras.layers.merge import concatenate, add
+from keras.layers import Input, Dense, Flatten, Reshape, add
 from keras.layers.advanced_activations import LeakyReLU
-from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import Conv2D, Conv2DTranspose
 from keras.layers import MaxPool2D
 from keras.optimizers import Adam
 from keras.initializers import RandomNormal
@@ -53,12 +52,21 @@ class Model(AutoEncoder):
 
     def upscale(self, filters):
         def block(x):
-            x = Conv2D(filters * 4, kernel_size=3, padding='same')(x)
-            x = LeakyReLU(0.1)(x)
-            x = PixelShuffler()(x)
+            x = Conv2D(filters=filters, kernel_size=(1, 1), strides=(1, 1), padding='same', activation='relu')(x)
+            x = Conv2DTranspose(filters=filters, kernel_size=(14, 14), strides=(2, 2), padding='same', activation='relu')(x)
+            x = Conv2D(filters=filters, kernel_size=(1, 1), strides=(1, 1), padding='same', activation='relu')(x)
             return x
         return block
     
+    def res_block(self, input_tensor, f):
+        x = input_tensor
+        x = Conv2D(f, kernel_size=3, kernel_initializer=conv_init, use_bias=False, padding="same")(x)
+        x = LeakyReLU(alpha=0.2)(x)
+        x = Conv2D(f, kernel_size=3, kernel_initializer=conv_init, use_bias=False, padding="same")(x)
+        x = add([x, input_tensor])
+        x = LeakyReLU(alpha=0.2)(x)
+        return x
+
     def Encoder(self):
         input_ = Input(shape=IMAGE_SHAPE)
         x = input_
@@ -79,66 +87,10 @@ class Model(AutoEncoder):
         x = self.upscale(128)(x)
         x = self.upscale(64)(x)
         x = self.upscale(32)(x)
-        x = model_EED(x)
-        x = Conv2D(3, kernel_size=1, padding='same', activation="tanh")(x)
+        x = self.upscale(16)(x)
+        x = Conv2D(3, kernel_size=5, padding='same', activation='sigmoid')(x)
+        x = self.res_block(x, 3)
+        x = self.res_block(x, 3)
+        x = self.res_block(x, 3)
+        x = self.res_block(x, 3)
         return KerasModel(input_, x)
-
-
-#from https://github.com/MarkPrecursor/EEDS-keras/blob/master/EED.py
-#Note that original EED is run on YCrCb images
-def Res_block(filters):
-    _input = Input(shape=(None, None, filters))
-
-    conv = Conv2D(filters=filters, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu')(_input)
-    conv = Conv2D(filters=filters, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='linear')(conv)
-
-    out = add(inputs=[_input, conv])
-    out = Activation('relu')(out)
-
-    model = KerasModel(inputs=_input, outputs=out)
-
-    return model
-
-
-def model_EED(Feature):
-
-    # Feature = Conv2D(filters=64, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu')(x)
-    Feature_out = Res_block(32)(Feature)
-
-    # Upsampling
-    Upsampling1 = Conv2D(filters=4, kernel_size=(1, 1), strides=(1, 1), padding='same', activation='relu')(Feature_out)
-    Upsampling2 = Conv2DTranspose(filters=4, kernel_size=(14, 14), strides=(2, 2),
-                                  padding='same', activation='relu')(Upsampling1)
-    Upsampling3 = Conv2D(filters=64, kernel_size=(1, 1), strides=(1, 1), padding='same', activation='relu')(Upsampling2)
-
-    # Mulyi-scale Reconstruction
-    Reslayer1 = Res_block(64)(Upsampling3)
-
-    Reslayer2 = Res_block(64)(Reslayer1)
-
-    # ***************//
-    Multi_scale1 = Conv2D(filters=16, kernel_size=(1, 1), strides=(1, 1), padding='same', activation='relu')(Reslayer2)
-
-    Multi_scale2a = Conv2D(filters=16, kernel_size=(1, 1), strides=(1, 1),
-                           padding='same', activation='relu')(Multi_scale1)
-
-    Multi_scale2b = Conv2D(filters=16, kernel_size=(1, 3), strides=(1, 1),
-                           padding='same', activation='relu')(Multi_scale1)
-    Multi_scale2b = Conv2D(filters=16, kernel_size=(3, 1), strides=(1, 1),
-                           padding='same', activation='relu')(Multi_scale2b)
-
-    Multi_scale2c = Conv2D(filters=16, kernel_size=(1, 5), strides=(1, 1),
-                           padding='same', activation='relu')(Multi_scale1)
-    Multi_scale2c = Conv2D(filters=16, kernel_size=(5, 1), strides=(1, 1),
-                           padding='same', activation='relu')(Multi_scale2c)
-
-    Multi_scale2d = Conv2D(filters=16, kernel_size=(1, 7), strides=(1, 1),
-                           padding='same', activation='relu')(Multi_scale1)
-    Multi_scale2d = Conv2D(filters=16, kernel_size=(7, 1), strides=(1, 1),
-                           padding='same', activation='relu')(Multi_scale2d)
-
-    Multi_scale2 = concatenate(inputs=[Multi_scale2a, Multi_scale2b, Multi_scale2c, Multi_scale2d])
-
-    #out = Conv2D(filters=1, kernel_size=(1, 1), strides=(1, 1), padding='same', activation='relu')(Multi_scale2)
-
-    return Multi_scale2
