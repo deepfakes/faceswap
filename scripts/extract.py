a7e5372daf9b4932280d7cb973036303c13e1090
torzdf <juhudatrop@2anom.com>
2018-04-03 01:02:23 +0100

    Alpha Build - Pre Testing

--- scripts/extract.py
+++ scripts/extract.py
@@ -13,6 +13,7 @@ from plugins.PluginLoader import PluginLoader
 
 class ExtractTrainingData(DirectoryProcessor):
     def create_parser(self, subparser, command, description):
+        self.optional_arguments = self.get_optional_arguments()
         self.parser = subparser.add_parser(
             command,
             help="Extract the faces from a pictures.",
@@ -21,74 +22,64 @@ class ExtractTrainingData(DirectoryProcessor):
             https://github.com/deepfakes/faceswap-playground"
             )
 
-    def add_optional_arguments(self, parser):
-        parser.add_argument('-D', '--detector',
-                            type=str,
-                            choices=("hog", "cnn", "all"), # case sensitive because this is used to load a plugin.
-                            default="hog",
-                            help="Detector to use. 'cnn' detects much more angles but will be much more resource intensive and may fail on large files.")
-
-        parser.add_argument('-l', '--ref_threshold',
-                            type=float,
-                            dest="ref_threshold",
-                            default=0.6,
-                            help="Threshold for positive face recognition"
-                            )
-
-        parser.add_argument('-n', '--nfilter',
-                            type=str,
-                            dest="nfilter",
-                            nargs='+',
-                            default="nfilter.jpg",
-                            help="Reference image for the persons you do not want to process. Should be a front portrait"
-                            )
-
-        parser.add_argument('-f', '--filter',
-                            type=str,
-                            dest="filter",
-                            nargs='+',
-                            default="filter.jpg",
-                            help="Reference image for the person you want to process. Should be a front portrait"
-                            )
-
-        parser.add_argument('-j', '--processes',
-                            type=int,
-                            default=1,
-                            help="Number of processes to use.")
-
-        parser.add_argument('-s', '--skip-existing',
-                            action='store_true',
-                            dest='skip_existing',
-                            default=False,
-                            help="Skips frames already extracted.")
-
-        parser.add_argument('-dl', '--debug-landmarks',
-                            action="store_true",
-                            dest="debug_landmarks",
-                            default=False,
-                            help="Draw landmarks for debug.")
-
-        parser.add_argument('-r', '--rotate-images',
-                            type=str,
-                            dest="rotate_images",
-                            default=None,
-                            help="If a face isn't found, rotate the images to try to find a face. Can find more faces at the "
+    @staticmethod
+    def get_optional_arguments():
+        ''' Put the arguments in a list so that they are accessible from both argparse and gui '''
+        argument_list = []
+        argument_list.append({ "opts": ('-D', '--detector'),
+                               "type": str,
+                               "choices": ("hog", "cnn", "all"), # case sensitive because this is used to load a plugin.
+                               "default": "hog",
+                               "help": "Detector to use. 'cnn' detects much more angles but will be much more resource intensive and may fail on large files."})
+        argument_list.append({ "opts": ('-l', '--ref_threshold'),
+                               "type": float,
+                               "dest": "ref_threshold",
+                               "default": 0.6,
+                               "help": "Threshold for positive face recognition"})
+        argument_list.append({ "opts": ('-n', '--nfilter'),
+                               "type": str,
+                               "dest": "nfilter",
+                               "nargs": '+',
+                               "default": "nfilter.jpg",
+                               "help": "Reference image for the persons you do not want to process. Should be a front portrait"})
+        argument_list.append({ "opts": ('-f', '--filter'),
+                               "type": str,
+                               "dest": "filter",
+                               "nargs": '+',
+                               "default": "filter.jpg",
+                               "help": "Reference image for the person you want to process. Should be a front portrait"})
+        argument_list.append({ "opts": ('-j', '--processes'),
+                               "type": int,
+                               "default": 1,
+                               "help": "Number of processes to use."})
+        argument_list.append({ "opts": ('-s', '--skip-existing'),
+                               "action": 'store_true',
+                               "dest": 'skip_existing',
+                               "default": False,
+                               "help": "Skips frames already extracted."})
+        argument_list.append({ "opts": ('-dl', '--debug-landmarks'),
+                               "action": "store_true",
+                               "dest": "debug_landmarks",
+                               "default": False,
+                               "help": "Draw landmarks for debug."})
+        argument_list.append({ "opts": ('-r', '--rotate-images'),
+                               "type": str,
+                               "dest": "rotate_images",
+                               "default": None,
+                               "help": "If a face isn't found, rotate the images to try to find a face. Can find more faces at the "
                                  "cost of extraction speed.  Pass in a single number to use increments of that size up to 360, "
-                                 "or pass in a list of numbers to enumerate exactly what angles to check.")
-
-        parser.add_argument('-ae', '--align-eyes',
-                            action="store_true",
-                            dest="align_eyes",
-                            default=False,
-                            help="Perform extra alignment to ensure left/right eyes lie at the same height")
-
-        parser.add_argument('-bt', '--blur-threshold',
-                            type=int,
-                            dest="blur_thresh",
-                            default=None,
-                            help="Automatically discard images blurrier than the specified threshold. Discarded images are moved into a \"blurry\" sub-folder. Lower values allow more blur")
-
-        return parser
+                                 "or pass in a list of numbers to enumerate exactly what angles to check."})
+        argument_list.append({ "opts": ('-ae', '--align-eyes'),
+                               "action": "store_true",
+                               "dest": "align_eyes",
+                               "default": False,
+                               "help": "Perform extra alignment to ensure left/right eyes lie at the same height"})
+        argument_list.append({ "opts": ('-bt', '--blur-threshold'),
+                               "type": int,
+                               "dest": "blur_thresh",
+                               "default": None,
+                               "help": "Automatically discard images blurrier than the specified threshold. Discarded images are moved into a \"blurry\" sub-folder. Lower values allow more blur"})
+        return argument_list
 
     def process(self):
         extractor_name = "Align" # TODO Pass as argument
