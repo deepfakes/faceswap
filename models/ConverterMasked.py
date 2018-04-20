72e1fe1557affffe40f4b456c678c09d6288521c
CyberDain <lepersorium@gmail.com>
2018-04-20 15:03:20 +0400

    fix mask

--- models/ConverterMasked.py
+++ models/ConverterMasked.py
@@ -21,7 +21,6 @@ class ConverterMasked(ConverterBase):
         
     #override
     def convert (self, img, img_face_landmarks, debug):
-        frame_mask_type = self.mask_type
         
         if debug:        
             debugs = [img.copy()]
@@ -49,14 +48,12 @@ class ConverterMasked(ConverterBase):
         img_prd_face_mask_3D = cv2.warpAffine( prd_face_mask, face_mat, img_size, np.zeros(img.shape, dtype=float), cv2.WARP_INVERSE_MAP | cv2.INTER_CUBIC )
         img_prd_face_mask_3D = np.clip (img_prd_face_mask_3D, 0.0, 1.0)
         
-        if np.argwhere(img_prd_face_mask_3D > 0.1).size == 0:
-            frame_mask_type == 'dst' #force to dst mask, if predicted mask is empty
         
-        if frame_mask_type == 'predicted':
+        if self.mask_type == 'predicted':
             img_mask_hard = img_prd_face_mask_3D
-        elif frame_mask_type == 'intersect':
+        elif self.mask_type == 'intersect':
             img_mask_hard = img_face_mask*img_prd_face_mask_3D #intersection              
-        elif frame_mask_type == 'dst':
+        elif self.mask_type == 'dst':
             img_mask_hard = img_face_mask
             img_mask_hard = np.repeat (img_mask_hard, (3,), -1)
 
@@ -65,36 +62,37 @@ class ConverterMasked(ConverterBase):
             
         img_mask_hard_copy = img_mask_hard.copy()
         img_mask_hard_copy[img_mask_hard_copy > 0.1] = 1.0
-        maxregion = np.argwhere(img_mask_hard_copy==1.0)
-        miny,minx = maxregion.min(axis=0)[:2]
-        maxy,maxx = maxregion.max(axis=0)[:2]
-        lenx = maxx - minx
-        leny = maxy - miny
-        masky = int(minx+(lenx//2))
-        maskx = int(miny+(leny//2))
-        lowest_len = min (lenx, leny)
-        ero = int( lowest_len * 0.085 )
-        blur = int( lowest_len * 0.10 )       
-        img_mask_blurry = cv2.erode(img_mask_hard, cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(ero,ero)), iterations = 1 )
-        img_mask_blurry = cv2.blur(img_mask_blurry, (blur, blur) )
-        
-        out_img = np.copy( img )
-        
-        if self.mode == 'hist-match':
-            prd_face = image_utils.color_hist_match(prd_face* prd_face_mask_1D, dst_face* prd_face_mask_1D)
-             
-        cv2.warpAffine( prd_face, face_mat, img_size, out_img, cv2.WARP_INVERSE_MAP | cv2.INTER_CUBIC, cv2.BORDER_TRANSPARENT )
-        
-        if debug:
-            debugs += [out_img.copy()]
-            debugs += [img_mask_blurry.copy()]
 
-        if self.mode == 'seamless':
-            out_img =  np.clip( img*(1-img_mask_hard) + (out_img*img_mask_hard) , 0, 1.0 )
-            out_img = cv2.seamlessClone( (out_img*255).astype(np.uint8), (img*255).astype(np.uint8), (img_mask_hard_copy*255).astype(np.uint8), (masky,maskx) , cv2.NORMAL_CLONE )
-            out_img = out_img.astype(np.float32) / 255.0
+        maxregion = np.argwhere(img_mask_hard_copy==1.0)        
+        out_img = np.copy( img )        
+        if maxregion.size != 0:
+            miny,minx = maxregion.min(axis=0)[:2]
+            maxy,maxx = maxregion.max(axis=0)[:2]
+            lenx = maxx - minx
+            leny = maxy - miny
+            masky = int(minx+(lenx//2))
+            maskx = int(miny+(leny//2))
+            lowest_len = min (lenx, leny)
+            ero = int( lowest_len * 0.085 )
+            blur = int( lowest_len * 0.10 )       
+            img_mask_blurry = cv2.erode(img_mask_hard, cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(ero,ero)), iterations = 1 )
+            img_mask_blurry = cv2.blur(img_mask_blurry, (blur, blur) )
+            
+            if self.mode == 'hist-match':
+                prd_face = image_utils.color_hist_match(prd_face* prd_face_mask_1D, dst_face* prd_face_mask_1D)
+                 
+            cv2.warpAffine( prd_face, face_mat, img_size, out_img, cv2.WARP_INVERSE_MAP | cv2.INTER_CUBIC, cv2.BORDER_TRANSPARENT )
+            
+            if debug:
+                debugs += [out_img.copy()]
+                debugs += [img_mask_blurry.copy()]
 
-        out_img =  np.clip( img*(1-img_mask_blurry) + (out_img*img_mask_blurry) , 0, 1.0 )
+            if self.mode == 'seamless':
+                out_img =  np.clip( img*(1-img_mask_hard) + (out_img*img_mask_hard) , 0, 1.0 )
+                out_img = cv2.seamlessClone( (out_img*255).astype(np.uint8), (img*255).astype(np.uint8), (img_mask_hard_copy*255).astype(np.uint8), (masky,maskx) , cv2.NORMAL_CLONE )
+                out_img = out_img.astype(np.float32) / 255.0
+
+            out_img =  np.clip( img*(1-img_mask_blurry) + (out_img*img_mask_blurry) , 0, 1.0 )
            
         if debug:
             debugs += [out_img]
