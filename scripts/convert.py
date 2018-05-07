9990017556e53fe3f61523131f4980fd10229bc2
Ivanovar-08 <ivanovar-08@mail.ru>
2018-05-07 13:17:15 +0300

    fixed field name to args

--- scripts/convert.py
+++ scripts/convert.py
@@ -131,7 +131,7 @@ class Convert(object):
         image = self.images.rotate_image(image, face.r)
         # TODO: This switch between 64 and 128 is a hack for now.
         # We should have a separate cli option for size        
-        size = 128 if (self.arguments.trainer.strip().lower() in ('gan128', 'originalhighres')) else 64        
+        size = 128 if (self.args.trainer.strip().lower() in ('gan128', 'originalhighres')) else 64        
         
         image = converter.patch_image(image,
                                       face,
