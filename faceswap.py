a2df83d1c2f2d9bb1ca0375d27207cca5aca87ec
LordOEQJen <39125942+LordOEQJen@users.noreply.github.com>
2018-05-13 03:53:30 +0200

    Load tools.py into the main faceswap.py gui
    File 1/2
    Adding sort and effmpeg to faceswap gui

--- faceswap.py
+++ faceswap.py
@@ -2,6 +2,8 @@
 """ The master faceswap.py script """
 import sys
 
+from tools.sort import SortProcessor
+from tools.effmpeg import Effmpeg
 import lib.cli as cli
 
 if sys.version_info[0] < 3:
@@ -25,7 +27,13 @@ if __name__ == "__main__":
         SUBPARSER, "train", "This command trains the model for the two faces A and B")
     CONVERT = cli.ConvertArgs(
         SUBPARSER, "convert", "Convert a source image to a new one with the face swapped")
-    GUIPARSERS = {'extract': EXTRACT, 'train': TRAIN, 'convert': CONVERT}
+    EFFMPEG = Effmpeg(
+            SUBPARSER, "effmpeg",
+            "This command allows you to easily execute common ffmpeg tasks.")
+    SORT = SortProcessor(
+            SUBPARSER, "sort",
+            "This command lets you sort images using various methods.")
+    GUIPARSERS = {'extract': EXTRACT, 'train': TRAIN, 'convert': CONVERT, 'effmpeg': EFFMPEG, 'sort': SORT}
     GUI = cli.GuiArgs(
         SUBPARSER, "gui", "Launch the Faceswap Graphical User Interface", GUIPARSERS)
     PARSER.set_defaults(func=bad_args)
