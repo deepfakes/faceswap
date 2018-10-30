5bb0b685f9159c45d80081b7139cf29ccc37fe54
torzdf <36920800+torzdf@users.noreply.github.com>
2018-10-30 16:44:54 +0000

    Configparse library added

--- lib/config.py
+++ lib/config.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-""" Default configurations for models """
+""" Default configurations for faceswap
+    Extends out configparser funcionality
+    by checking for default config updates
+    and returning data in it's correct format """
 
 import os
 import sys
@@ -20,8 +23,36 @@ class FaceswapConfig():
         self.handle_config()
 
     def set_defaults(self):
-        """ Override for plugin specific config defaults """
+        """ Override for plugin specific config defaults
+
+            Should be a series of self.add_section() and self.add_item() calls
+
+            e.g:
+
+            section = "sect_1"
+            self.add_section(title=section,
+                         info="Section 1 Information")
+
+            self.add_item(section=section,
+                          title="option_1",
+                          datatype=bool,
+                          default=False,
+                          info="sect_1 option_1 information")
+        """
         raise NotImplementedError
+
+    def get(self, section, option):
+        """ Return a config item in it's correct format """
+        datatype = self.defaults[section][option]["type"]
+        if datatype == bool:
+            func = self.config.getboolean
+        elif datatype == int:
+            func = self.config.getint
+        elif datatype == float:
+            func = self.config.getfloat
+        else:
+            func = self.config.get
+        return func(section, option)
 
     def get_config_file(self):
         """ Return the config file from the calling folder """
@@ -36,7 +67,8 @@ class FaceswapConfig():
         self.defaults[title] = OrderedDict()
         self.defaults[title]["helptext"] = info
 
-    def add_item(self, section=None, title=None, default=None, info=None):
+    def add_item(self, section=None, title=None, datatype=str,
+                 default=None, info=None):
         """ Add a default item to a config section """
         if None in (section, title, default, info):
             raise ValueError("Default config items must have a section, "
@@ -44,8 +76,12 @@ class FaceswapConfig():
                              "information text")
         if not self.defaults.get(section, None):
             raise ValueError("Section does not exist: {}".format(section))
+        if datatype not in (str, bool, float, int):
+            raise ValueError("Datatype must be one of str, bool, float or "
+                             "int: {} - {}".format(section, title))
         self.defaults[section][title] = {"default": default,
-                                         "helptext": info}
+                                         "helptext": info,
+                                         "type": datatype}
 
     def check_exists(self):
         """ Check that a config file exists """
@@ -55,20 +91,31 @@ class FaceswapConfig():
 
     def create_default(self):
         """ Generate a default config if it does not exist """
-        print("Generating default config at: {}".format(self.configfile))
-        f_cfgfile = open(self.configfile, "w")
         for section, items in self.defaults.items():
-            helptext = self.format_help(items["helptext"], is_section=True)
-            self.config.add_section(section)
-            self.config.set(section, helptext)
-            for item, value in items.items():
+            self.insert_config_section(section, items["helptext"])
+            for item, opt in items.items():
                 if item == "helptext":
                     continue
-                helptext = self.format_help(value["helptext"])
-                self.config.set(section, helptext)
-                self.config.set(section, item, str(value["default"]))
-        self.config.write(f_cfgfile)
-        f_cfgfile.close()
+                self.insert_config_item(section,
+                                        item,
+                                        opt["default"],
+                                        opt["helptext"])
+        self.save_config()
+
+    def insert_config_section(self, section, helptext, config=None):
+        """ Insert a section into the config """
+        config = self.config if config is None else config
+        helptext = self.format_help(helptext, is_section=True)
+        config.add_section(section)
+        config.set(section, helptext)
+
+    def insert_config_item(self, section, item, default, helptext,
+                           config=None):
+        """ Insert an item into a config section """
+        config = self.config if config is None else config
+        helptext = self.format_help(helptext, is_section=False)
+        config.set(section, helptext)
+        config.set(section, item, str(default))
 
     @staticmethod
     def format_help(helptext, is_section=False):
@@ -84,8 +131,52 @@ class FaceswapConfig():
         """ Load values from config """
         self.config.read(self.configfile)
 
+    def save_config(self):
+        """ Save a config file """
+        print("Saving config at: {}".format(self.configfile))
+        f_cfgfile = open(self.configfile, "w")
+        self.config.write(f_cfgfile)
+        f_cfgfile.close()
+
+    def validate_config(self):
+        """ Check for options in default config against saved config
+            and add/remove as appropriate """
+        if not self.check_config_change():
+            return
+        new_config = ConfigParser(allow_no_value=True)
+        for section, items in self.defaults.items():
+            self.insert_config_section(section, items["helptext"], new_config)
+            for item, opt in items.items():
+                if item == "helptext":
+                    continue
+                if section not in self.config.sections():
+                    opt_value = opt["default"]
+                else:
+                    opt_value = self.config[section].get(item, opt["default"])
+                self.insert_config_item(section,
+                                        item,
+                                        opt_value,
+                                        opt["helptext"],
+                                        new_config)
+        self.config = new_config
+        self.save_config()
+
+    def check_config_change(self):
+        """ Check whether new default items have been added or removed
+            from the config file compared to saved version """
+        if set(self.config.sections()) != set(self.defaults.keys()):
+            return True
+
+        for section, items in self.defaults.items():
+            opts = [opt for opt in items.keys() if opt != "helptext"]
+            if set(self.config.options(section)) != set(opts):
+                return True
+        return False
+
     def handle_config(self):
         """ Handle the config """
         if not self.check_exists():
             self.create_default()
         self.load_config()
+        self.validate_config()
+        exit(0)
--- plugins/train/_config.py
+++ plugins/train/_config.py
@@ -7,73 +7,55 @@ from lib.config import FaceswapConfig
 class Config(FaceswapConfig):
     """ Config File for Models """
 
-    @staticmethod
-    def comments():
-        """ Comments for the model options """
-        d_comments = dict()
-        d_comments["Global"] = {
-            "00_main": "Options that apply to all models",
-            "dssim_loss": ""}
-        return d_comments
-
     def set_defaults(self):
         """ Set the default values for config """
         # << GLOBAL OPTIONS >> #
         section = "Global"
         self.add_section(title=section,
                          info="Options that apply to all models")
+
         self.add_item(
-            section=section,
-            title="dssim_loss",
-            default=False,
-            info="Use DSSIM for Loss rather than Mean Absolute Error\nMay "
-                 "increase overall quality.")
+            section=section, title="dssim_loss", datatype=bool, default=False,
+            info="Use DSSIM for Loss rather than Mean Absolute Error\n"
+                 "May increase overall quality.")
 
         # << ORIGINAL HIRES MODEL OPTIONS >> #
         section = "Original_HiRes"
-        self.add_section(
-            title=section,
-            info="Higher resolution version of the Original Model")
+        self.add_section(title=section,
+                         info="Higher resolution version of the Original "
+                              "Model")
+
         self.add_item(
-            section=section,
-            title="encoder_type",
+            section=section, title="encoder_type", datatype=str,
             default="ORIGINAL",
             info="Encoder type to use. Choose from:\n"
                  "ORIGINAL: Basic encoder for this model type\n"
                  "STANDARD: New, balanced encoder. More memory consuming\n"
                  "HIGHRES: High resolution tensors optimized encoder: 176x+")
         self.add_item(
-            section=section,
-            title="nodes",
-            default=1024,
+            section=section, title="nodes", datatype=int, default=1024,
             info="Number of nodes for decoder. Don't change this unless you "
                  "know what you are doing!")
         self.add_item(
-            section=section,
-            title="image_size",
-            default=128,
+            section=section, title="image_size", datatype=int, default=128,
             info="Number of pixels for face width and height. Don't change "
                  "this unless you know what you are doing!")
         self.add_item(
-            section=section,
-            title="complexity_encoder",
+            section=section, title="complexity_encoder", datatype=int,
             default=128,
             info="Encoder Convolution Layer Complexity. sensible ranges: "
                  "128 to 160")
         self.add_item(
-            section=section,
-            title="complexity_decoder_a",
+            section=section, title="complexity_decoder_a", datatype=int,
             default=384,
             info="Decoder A Complexity. Only applicable for STANDARD and "
                  "ORIGINAL encoders")
         self.add_item(
-            section=section,
-            title="complexity_decoder_b",
+            section=section, title="complexity_decoder_b", datatype=int,
             default=512,
             info="Decoder B Complexity. Only applicable for STANDARD and "
                  "ORIGINAL encoders")
         self.add_item(
-            section=section,
-            title="subpixel_upscaling",
+            section=section, title="subpixel_upscaling", datatype=bool,
             default=False,
             info="Might increase upscaling quality at cost of VRAM")
