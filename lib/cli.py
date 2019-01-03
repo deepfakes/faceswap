8b1761486fe6460d9d755e12202b42abf0261a42
kvrooman <vrooman.kyle@gmail.com>
2019-01-03 09:07:59 -0600

    Consolidation of converters & refactor
    Initial Upload of alpha
    
    Items
    - consolidate convert_mased & convert_adjust into one converter
    -add average color adjust to convert_masked
    -allow mask transition blur size to be a fixed integer of pixels and a fraction of the facial mask size
    -allow erosion/dilation size to be a fixed integer of pixels and a fraction of the facial mask size
    -eliminate redundant type conversions to avoid multiple round-off errors
    -refactor loops for vectorization/speed
    -reorganize for clarity & style changes
    
    TODO
    - bug/issues with warping the new face onto a transparent old image...use a cleanup mask for now
    - issues with mask border giving black ring at zero erosion .. investigate
    - remove GAN ??
    - test enlargment factors of umeyama standard face .. match to coverage factor
    - make enlargment factor a model parameter
    - remove convert_adjusted and referencing code when finished

--- lib/cli.py
+++ lib/cli.py
@@ -147,6 +147,13 @@ class FileFullPaths(FullPaths):
         return [(name, getattr(self, name)) for name in names]
 
 
+class DirOrFileFullPaths(FileFullPaths):
+    """ Class that the gui uses to determine that the input can take a folder or a filename.
+        Inherits functionality from FileFullPaths
+        Has the effect of giving the user 2 Open Dialogue buttons in the gui """
+    pass
+
+
 class SaveFileFullPaths(FileFullPaths):
     """
     Class that gui uses to determine if you need to save a file.
@@ -306,12 +313,14 @@ class ExtractConvertArgs(FaceSwapArgs):
         argparse and gui """
         argument_list = list()
         argument_list.append({"opts": ("-i", "--input-dir"),
-                              "action": DirFullPaths,
+                              "action": DirOrFileFullPaths,
+                              "filetypes": "video",
                               "dest": "input_dir",
                               "default": "input",
-                              "help": "Input directory. A directory "
-                                      "containing the files you wish to "
-                                      "process. Defaults to 'input'"})
+                              "help": "Input directory or video. Either a "
+                                      "directory containing the image files "
+                                      "you wish to process or path to a "
+                                      "video file. Defaults to 'input'"})
         argument_list.append({"opts": ("-o", "--output-dir"),
                               "action": DirFullPaths,
                               "dest": "output_dir",
@@ -551,19 +560,25 @@ class ConvertArgs(ExtractConvertArgs):
                               "default": "Masked",
                               "help": "Converter to use"})
         argument_list.append({"opts": ("-b", "--blur-size"),
-                              "type": int,
+                              "type": float,
                               "default": 2,
-                              "help": "Blur size. (Masked converter only)"})
+                              "help": "Blur size for smoothing the transition "
+                                      "between the swapped face and the "
+                                      "background image(Masked converter only)"
+                                      "integer values will blur x pixels,"
+                                      "fractions will blur x% of the face area"})
         argument_list.append({"opts": ("-e", "--erosion-kernel-size"),
                               "dest": "erosion_kernel_size",
-                              "type": int,
+                              "type": float,
                               "default": None,
                               "help": "Erosion kernel size. Positive values "
                                       "apply erosion which reduces the edge "
                                       "of the swapped face. Negative values "
                                       "apply dilation which allows the "
                                       "swapped face to cover more space. "
-                                      "(Masked converter only)"})
+                                      "(Masked converter only)"
+                                      "integer values will erode x pixels,"
+                                      "fractions will erode x% of the face area"})
         argument_list.append({"opts": ("-M", "--mask-type"),
                               # lowercase this, because it's just a
                               # string later on.
--- plugins/convert/Convert_Masked.py
+++ plugins/convert/Convert_Masked.py
@@ -15,216 +15,197 @@ logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 
 class Convert():
     def __init__(self, encoder, trainer,
-                 blur_size=2, seamless_clone=False, mask_type="facehullandrect",
-                 erosion_kernel_size=None, match_histogram=False, sharpen_image=None,
-                 draw_transparent=False, **kwargs):
+                 blur_size=0, seamless_clone=False, mask_type="facehullandrect",
+                 erosion_kernel_size=0, match_histogram=False, sharpen_image=None,
+                 draw_transparent=False, avg_color_adjust=False, **kwargs):
         self.encoder = encoder
         self.trainer = trainer
-        self.erosion_kernel = None
-        self.erosion_kernel_size = erosion_kernel_size
-        if erosion_kernel_size is not None:
-            if erosion_kernel_size > 0:
-                self.erosion_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,
-                                                                (erosion_kernel_size,
-                                                                 erosion_kernel_size))
-            elif erosion_kernel_size < 0:
-                self.erosion_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,
-                                                                (abs(erosion_kernel_size),
-                                                                 abs(erosion_kernel_size)))
         self.blur_size = blur_size
-        self.seamless_clone = seamless_clone
         self.sharpen_image = sharpen_image
         self.match_histogram = match_histogram
-        self.mask_type = mask_type.lower()  # Choose in 'FaceHullAndRect', 'FaceHull', 'Rect'
+        self.mask_type = mask_type.lower()
         self.draw_transparent = draw_transparent
-
+        self.avg_color_adjust = avg_color_adjust
+        self.erosion_kernel_size = erosion_kernel_size
+        self.seamless_clone = False if draw_transparent else seamless_clone
+        self.erosion_size = 0 if erosion_kernel_size is None else erosion_kernel_size
+        if self.erosion_size >= 1:
+            e_size = (int(abs(self.erosion_size)),int(abs(self.erosion_size)))
+            self.erosion_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,
+                                                            e_size)
+            
     def patch_image(self, image, face_detected, size):
-
         image_size = image.shape[1], image.shape[0]
-
-        mat = numpy.array(get_align_mat(face_detected,
-                                        size,
-                                        should_align_eyes=False)).reshape(2, 3)
-
-        if "GAN" not in self.trainer:
-            mat = mat * size
-        else:
-            padding = int(48/256*size)
-            mat = mat * (size - 2 * padding)
-            mat[:, 2] += padding
-
+        mat = get_align_mat(face_detected,size,False).reshape(2, 3)
+        
+        # insert Field of View Logic here to modify alignment mat
+        # test various enlargment factors to umeyama's standard face
+        
+        enlargement_factor = 0      # @ eyebrow ends
+        #enlargement_factor = 12/256
+        #enlargement_factor = 24/256  @ ears    .. coverage = 180 test more
+        #enlargement_factor = 48/256  @ mugshot .. coverage = 220
+        padding = int(enlargement_factor*size)
+        mat = mat * (size - 2 * padding)
+        mat[:, 2] += padding
+            
         new_face = self.get_new_face(image, mat, size)
+        image_mask = self.get_image_mask(image, mat, image_size, new_face,
+                                         face_detected.landmarks_as_xy)
+        patched_face = self.apply_new_face(image, mat, size, image_size, new_face, image_mask)
+        
+        return patched_face
 
-        image_mask = self.get_image_mask(image,
-                                         new_face,
-                                         face_detected.landmarks_as_xy,
-                                         mat,
-                                         image_size)
+    def get_new_face(self, image, mat, size):
+        face = cv2.warpAffine(image.astype('float32'), mat, (size, size))
+        face = numpy.expand_dims(face, 0)
+        numpy.clip(face, 0.0, 255.0, out=face)
+        new_face = None
+        mask = None
+        
+        if 'GAN' in self.trainer:
+            normalized_face = face / 255.0 * 2.0 - 1.0
+            fake_output = self.encoder(normalized_face)
+            if "128" in self.trainer:
+                fake_output = fake_output[0]
+            mask = fake_output[:, :, :, :1]
+            new_face = fake_output[:, :, :, 1:]
+            new_face = mask * new_face + (1.0 - mask) * normalized_face
+            new_face = (new_face[0] + 1.0) / 2.0
+        else:
+            normalized_face = face / 255.0
+            new_face = self.encoder(normalized_face)[0]
+            
+        numpy.clip(new_face * 255.0, 0.0, 255.0, out=new_face)
+        return new_face
 
-        return self.apply_new_face(image, new_face, image_mask, mat, image_size, size)
-
-    @staticmethod
-    def convert_transparent(image, new_face, image_mask, image_size):
-        """ Add alpha channels to images and change to
-            transparent background """
-        image = numpy.zeros((image_size[1], image_size[0], 4),
-                            dtype=numpy.uint8)
-        image_mask = add_alpha_channel(image_mask, 100)
-        new_face = add_alpha_channel(new_face, 100)
-        return image, new_face, image_mask
-
-    def apply_new_face(self, image, new_face, image_mask, mat, image_size, size):
-
-        if self.draw_transparent:
-            image, new_face, image_mask = self.convert_transparent(image,
-                                                                   new_face,
-                                                                   image_mask,
-                                                                   image_size)
-            self.seamless_clone = False  # Alpha channel not supported in seamless
-        base_image = numpy.copy(image)
-        new_image = numpy.copy(image)
-
-        cv2.warpAffine(new_face,
-                       mat,
-                       image_size,
-                       new_image,
-                       cv2.WARP_INVERSE_MAP | cv2.INTER_CUBIC,
-                       cv2.BORDER_TRANSPARENT)
-
-        if self.sharpen_image == "bsharpen":
-            # Sharpening using filter2D
-            kernel = numpy.ones((3, 3)) * (-1)
-            kernel[1, 1] = 9
-            new_image = cv2.filter2D(new_image, -1, kernel)
-        elif self.sharpen_image == "gsharpen":
-            # Sharpening using Weighted Method
-            gaussain_blur = cv2.GaussianBlur(new_image, (0, 0), 3.0)
-            new_image = cv2.addWeighted(
-                new_image, 1.5, gaussain_blur, -0.5, 0, new_image)
-
+    def get_image_mask(self, image, mat, image_size, new_face, landmarks):
+        if 'rect' in self.mask_type:
+            face_src = numpy.ones(new_face.shape, dtype='float32')
+            image_mask = cv2.warpAffine(face_src, mat, image_size,
+                                        flags = cv2.WARP_INVERSE_MAP |
+                                                cv2.INTER_NEAREST,
+                                        borderMode = cv2.BORDER_CONSTANT,
+                                        borderValue = 0.0)
+                                        
+        if 'hull' in self.mask_type:
+            hull_mask = numpy.zeros(image.shape, dtype='float32')
+            hull = cv2.convexHull(numpy.array(landmarks).reshape((-1, 2)))
+            cv2.fillConvexPoly(hull_mask, hull, (1.0, 1.0, 1.0), lineType = cv2.LINE_AA)
+            image_mask = image_mask * hull_mask if 'and' in self.mask_type else hull_mask
+        
+        numpy.nan_to_num(image_mask, copy=False)
+        numpy.clip(image_mask, 0.0, 1.0, out=image_mask)
+        if self.erosion_size != 0:
+            if self.erosion_size < 1:
+                mask_radius = numpy.sqrt(numpy.sum(image_mask)) / 2
+                percent_erode = max(1,int(abs(self.erosion_size * mask_radius)))
+                self.erosion_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,
+                                                                (percent_erode,
+                                                                percent_erode))
+            args = {'src':image_mask, 'kernel':self.erosion_kernel, 'iterations':1}
+            image_mask = cv2.erode(**args) if self.erosion_size > 0 else cv2.dilate(**args)
+            
+        if self.blur_size != 0:
+            if self.blur_size < 1.0:
+                mask_radius = numpy.sqrt(numpy.sum(image_mask)) / 2
+                self.blur_size = max(1,int(self.blur_size * mask_radius))
+            image_mask = cv2.blur(image_mask, (self.blur_size,self.blur_size))
+            
+        numpy.clip(image_mask, 0.0, 1.0, out=image_mask)
+        return image_mask
+        
+    def apply_new_face(self, image, mat, size, image_size, new_face, image_mask):
         outimage = None
+        if self.draw_transparent:
+            new_face = add_alpha_channel(new_face, 100)
+            image_mask = add_alpha_channel(image_mask, 100)
+            image = numpy.zeros((image_size[1],image_size[0], 4), dtype='uint8')
+        
+        # bug / issues with transparent border not working
+        # test further , cleanup mask replace for now
+        new_image = cv2.warpAffine(new_face, mat, image_size,
+                                   flags = cv2.WARP_INVERSE_MAP | cv2.INTER_CUBIC,
+                                   borderMode = cv2.BORDER_CONSTANT,
+                                   borderValue = (-1.0,-1.0,-1.0))
+                                   
+        cleanup_mask = (new_image == (-1.0,-1.0,-1.0))
+        new_image[cleanup_mask] = image[cleanup_mask]
+                       
+        if self.sharpen_image is not None:
+            numpy.clip(new_image, 0.0, 255.0, out=new_image)
+            if self.sharpen_image == "bsharpen":
+                kernel = numpy.ones((3, 3)) * (-1)
+                kernel[1, 1] = 9
+                new_image = cv2.filter2D(new_image, -1, kernel)
+            elif self.sharpen_image == "gsharpen":
+                blur = cv2.GaussianBlur(new_image, (0, 0), 3.0)
+                new_image = cv2.addWeighted(new_image, 1.5, blur, -0.5, 0, new_image)
+        
+        if self.avg_color_adjust:
+            for iterations in [0,1]:
+                numpy.clip(new_image, 0.0, 255.0, out=new_image)
+                old_avgs = numpy.average(image * image_mask, axis=(0,1))
+                new_avgs = numpy.average(new_image * image_mask, axis=(0,1))
+                diff = old_avgs - new_avgs
+                new_image = new_image + diff  
+                
+        if self.match_histogram:
+            numpy.clip(new_image, 0.0, 255.0, out=new_image)
+            new_image = self.color_hist_match(new_image,
+                                              image,
+                                              image_mask)
+                                              
         if self.seamless_clone:
-            unitMask = numpy.clip(image_mask * 365, 0, 255).astype(numpy.uint8)
+            unitMask = numpy.clip(image_mask * 365, 0, 255).astype('uint8')
             logger.info(unitMask.shape)
-            logger.info(new_image.shape)
-            logger.info(base_image.shape)
+            logger.info(image.shape)
             maxregion = numpy.argwhere(unitMask == 255)
-
             if maxregion.size > 0:
                 miny, minx = maxregion.min(axis=0)[:2]
                 maxy, maxx = maxregion.max(axis=0)[:2]
                 lenx = maxx - minx
                 leny = maxy - miny
-                masky = int(minx + (lenx // 2))
-                maskx = int(miny + (leny // 2))
-                outimage = cv2.seamlessClone(new_image.astype(numpy.uint8),
-                                             base_image.astype(numpy.uint8),
+                mask = int(minx + (lenx // 2)),  int(miny + (leny // 2))
+                outimage = cv2.seamlessClone(new_image.astype('uint8'),
+                                             image,
                                              unitMask,
-                                             (masky, maskx),
+                                             mask,
                                              cv2.NORMAL_CLONE)
-                return outimage
-
-        foreground = cv2.multiply(image_mask, new_image.astype(float))
-        background = cv2.multiply(1.0 - image_mask, base_image.astype(float))
-        outimage = cv2.add(foreground, background)
-
-        return outimage
-
+        else:
+            foreground = image_mask * new_image
+            background = ( 1.0 - image_mask ) * image.astype('float32')
+            outimage = foreground + background
+        
+        numpy.clip(outimage, 0.0, 255.0, out=outimage)
+        return outimage.astype('uint8')
+        
+    def color_hist_match(self, source, target, mask):
+        for channel in [0,1,2]:
+            source[:, :, channel] = self.hist_match(source[:, :, channel],
+                                                    target[:, :, channel],
+                                                    mask[:, :, channel])
+        return source
+        
     def hist_match(self, source, template, mask=None):
         # Code borrowed from:
         # https://stackoverflow.com/questions/32655686/histogram-matching-of-two-images-in-python-2-x
-        masked_source = source
-        masked_template = template
-
         if mask is not None:
-            masked_source = source * mask
-            masked_template = template * mask
-
-        oldshape = source.shape
+            source = source * mask
+            template = template * mask
+        
+        outshape = source.shape
         source = source.ravel()
         template = template.ravel()
-        masked_source = masked_source.ravel()
-        masked_template = masked_template.ravel()
-        s_values, bin_idx, s_counts = numpy.unique(source, return_inverse=True,
+        s_values, bin_idx, s_counts = numpy.unique(source,
+                                                   return_inverse=True,
                                                    return_counts=True)
         t_values, t_counts = numpy.unique(template, return_counts=True)
-        ms_values, mbin_idx, ms_counts = numpy.unique(source, return_inverse=True,
-                                                      return_counts=True)
-        mt_values, mt_counts = numpy.unique(template, return_counts=True)
-
-        s_quantiles = numpy.cumsum(s_counts).astype(numpy.float64)
-        s_quantiles /= s_quantiles[-1]
-        t_quantiles = numpy.cumsum(t_counts).astype(numpy.float64)
-        t_quantiles /= t_quantiles[-1]
-        interp_t_values = numpy.interp(s_quantiles, t_quantiles, t_values)
-
-        return interp_t_values[bin_idx].reshape(oldshape)
-
-    def color_hist_match(self, src_im, tar_im, mask):
-        matched_R = self.hist_match(src_im[:, :, 0], tar_im[:, :, 0], mask)
-        matched_G = self.hist_match(src_im[:, :, 1], tar_im[:, :, 1], mask)
-        matched_B = self.hist_match(src_im[:, :, 2], tar_im[:, :, 2], mask)
-        matched = numpy.stack((matched_R, matched_G, matched_B), axis=2).astype(src_im.dtype)
-        return matched
-
-    def get_new_face(self, image, mat, size):
-        face = cv2.warpAffine(image, mat, (size, size))
-        face = numpy.expand_dims(face, 0)
-        face_clipped = numpy.clip(face[0], 0, 255).astype(image.dtype)
-        new_face = None
-        mask = None
-
-        if "GAN" not in self.trainer:
-            normalized_face = face / 255.0
-            new_face = self.encoder(normalized_face)[0]
-            new_face = numpy.clip(new_face * 255, 0, 255).astype(image.dtype)
-        else:
-            normalized_face = face / 255.0 * 2 - 1
-            fake_output = self.encoder(normalized_face)
-            if "128" in self.trainer:  # TODO: Another hack to switch between 64 and 128
-                fake_output = fake_output[0]
-            mask = fake_output[:, :, :, :1]
-            new_face = fake_output[:, :, :, 1:]
-            new_face = mask * new_face + (1 - mask) * normalized_face
-            new_face = numpy.clip((new_face[0] + 1) * 255 / 2, 0, 255).astype(image.dtype)
-
-        if self.match_histogram:
-            new_face = self.color_hist_match(new_face, face_clipped, mask)
-
-        return new_face
-
-    def get_image_mask(self, image, new_face, landmarks, mat, image_size):
-
-        face_mask = numpy.zeros(image.shape, dtype=float)
-        if 'rect' in self.mask_type:
-            face_src = numpy.ones(new_face.shape, dtype=float)
-            cv2.warpAffine(face_src,
-                           mat,
-                           image_size,
-                           face_mask,
-                           cv2.WARP_INVERSE_MAP | cv2.INTER_CUBIC, cv2.BORDER_TRANSPARENT)
-
-        hull_mask = numpy.zeros(image.shape, dtype=float)
-        if 'hull' in self.mask_type:
-            hull = cv2.convexHull(
-                numpy.array(landmarks).reshape((-1, 2)).astype(int)).flatten().reshape((-1, 2))
-            cv2.fillConvexPoly(hull_mask, hull, (1, 1, 1))
-
-        if self.mask_type == 'rect':
-            image_mask = face_mask
-        elif self.mask_type == 'facehull':
-            image_mask = hull_mask
-        else:
-            image_mask = ((face_mask*hull_mask))
-
-        if self.erosion_kernel is not None:
-            if self.erosion_kernel_size > 0:
-                image_mask = cv2.erode(image_mask, self.erosion_kernel, iterations=1)
-            elif self.erosion_kernel_size < 0:
-                dilation_kernel = abs(self.erosion_kernel)
-                image_mask = cv2.dilate(image_mask, dilation_kernel, iterations=1)
-
-        if self.blur_size != 0:
-            image_mask = cv2.blur(image_mask, (self.blur_size, self.blur_size))
-
-        return image_mask
+        s_quants = numpy.cumsum(s_counts, dtype='float32')
+        t_quants = numpy.cumsum(t_counts, dtype='float32')
+        s_quants /= s_quants[-1]
+        t_quants /= t_quants[-1]
+        interp_t_values = numpy.interp(s_quants, t_quants, t_values).astype('float32')
+        source = interp_t_values[bin_idx].reshape(outshape)
+        
+        return source
