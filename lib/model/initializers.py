0cfd507688ea7f1cc8bca878de3f9e291001bee6
kvrooman <vrooman.kyle@gmail.com>
2019-01-14 02:53:03 -0600

    Code to use ICNR

--- lib/model/initializers.py
+++ lib/model/initializers.py
@@ -4,6 +4,7 @@
         shoanlu GAN: https://github.com/shaoanlu/faceswap-GAN"""
 
 import tensorflow as tf
+from keras.initializers import Initializer
 
 
 def icnr_keras(shape, dtype=None):
@@ -23,3 +24,40 @@ def icnr_keras(shape, dtype=None):
     var_x = tf.space_to_depth(var_x, block_size=scale)
     var_x = tf.transpose(var_x, perm=[1, 2, 0, 3])
     return var_x
+    
+    
+    
+class ICNR_init(Initializer):
+    '''
+    ICNR initializer for checkerboard artifact free sub pixel convolution
+
+    Andrew Aitken et al. Checkerboard artifact free sub-pixel convolution
+    https://arxiv.org/pdf/1707.02937.pdf	https://distill.pub/2016/deconv-checkerboard/
+
+    Parameters:
+        initializer: initializer used for sub kernels (orthogonal, glorot uniform, etc.)
+        scale: scale factor of sub pixel convolution (upsampling from 8x8 to 16x16 is scale 2)
+    Return:
+        The modified kernel weights
+    Example:
+        x = conv2d(... weights_initializer=ICNR(initializer=he_uniform(), scale=2))
+    '''
+
+    def __init__(self, initializer, scale=2):
+        self.scale = scale
+        self.initializer = initializer
+
+    def __call__(self, shape, dtype='float32'):  # tf needs partition_info=None
+        shape = list(shape)
+        if self.scale == 1:
+            return self.initializer(shape)
+        new_shape = shape[:3] + [shape[3] // (self.scale ** 2)]
+        x = self.initializer(new_shape, dtype)
+        x = tf.transpose(x, perm=[2, 0, 1, 3])
+        x = tf.image.resize_nearest_neighbor(x, size=(shape[0] * self.scale, shape[1] * self.scale), align_corners=True)
+        x = tf.space_to_depth(x, block_size=self.scale, data_format='NHWC')
+        x = tf.transpose(x, perm=[1, 2, 0, 3])
+        return x
+
+    def get_config(self):
+        return {'scale': self.value, 'initializer': self.initializer}
