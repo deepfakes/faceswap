2108c7f437eab53d1444443a4fd9582000d5ef20
khilos <vrooman.kyle@gmail.com>
2019-04-01 21:34:09 -0500

    Pooling Layers for Model
    Pooling Layers for Model

--- lib/model/layers.py
+++ lib/model/layers.py
@@ -17,6 +17,7 @@ from keras.utils import conv_utils
 from keras.utils.generic_utils import get_custom_objects
 from keras import initializers
 from keras.layers import ZeroPadding2D
+from keras.layers.pooling import _GlobalPooling2D
 
 
 class PixelShuffler(Layer):
@@ -331,6 +332,69 @@ class ReflectionPadding2D(Layer):
         base_config = super(ReflectionPadding2D, self).get_config()
         return dict(list(base_config.items()) + list(config.items())) 
 
+
+class GlobalMinPooling2D(_GlobalPooling2D):
+    """Global minimum pooling operation for spatial data.
+    # Arguments
+        data_format: A string,
+            one of `channels_last` (default) or `channels_first`.
+            The ordering of the dimensions in the inputs.
+            `channels_last` corresponds to inputs with shape
+            `(batch, height, width, channels)` while `channels_first`
+            corresponds to inputs with shape
+            `(batch, channels, height, width)`.
+            It defaults to the `image_data_format` value found in your
+            Keras config file at `~/.keras/keras.json`.
+            If you never set it, then it will be "channels_last".
+    # Input shape
+        - If `data_format='channels_last'`:
+            4D tensor with shape:
+            `(batch_size, rows, cols, channels)`
+        - If `data_format='channels_first'`:
+            4D tensor with shape:
+            `(batch_size, channels, rows, cols)`
+    # Output shape
+        2D tensor with shape:
+        `(batch_size, channels)`
+    """
+
+    def call(self, inputs):
+        if self.data_format == 'channels_last':
+            return K.min(inputs, axis=[1, 2])
+        else:
+            return K.min(inputs, axis=[2, 3])
+
+
+class GlobalStdDevPooling2D(_GlobalPooling2D):
+    """Global standard deviation pooling operation for spatial data.
+    # Arguments
+        data_format: A string,
+            one of `channels_last` (default) or `channels_first`.
+            The ordering of the dimensions in the inputs.
+            `channels_last` corresponds to inputs with shape
+            `(batch, height, width, channels)` while `channels_first`
+            corresponds to inputs with shape
+            `(batch, channels, height, width)`.
+            It defaults to the `image_data_format` value found in your
+            Keras config file at `~/.keras/keras.json`.
+            If you never set it, then it will be "channels_last".
+    # Input shape
+        - If `data_format='channels_last'`:
+            4D tensor with shape:
+            `(batch_size, rows, cols, channels)`
+        - If `data_format='channels_first'`:
+            4D tensor with shape:
+            `(batch_size, channels, rows, cols)`
+    # Output shape
+        2D tensor with shape:
+        `(batch_size, channels)`
+    """
+
+    def call(self, inputs):
+        if self.data_format == 'channels_last':
+            return K.std(inputs, axis=[1, 2])
+        else:
+            return K.std(inputs, axis=[2, 3])
 
 # Update layers into Keras custom objects
 for name, obj in inspect.getmembers(sys.modules[__name__]):
