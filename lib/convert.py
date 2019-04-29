66364f8d65c9da7bf80b9a86acf49d7bad26dad3
khilos <vrooman.kyle@gmail.com>
2019-04-29 07:33:46 -0500

    convert modifications

--- lib/convert.py
+++ lib/convert.py
@@ -7,8 +7,8 @@ import logging
 
 import cv2
 import numpy as np
-from lib.model import masks as model_masks
-
+from lib.model.masks import get_default_mask
+from lib.model.masks import Mask as mask_class
 from plugins.plugin_loader import PluginLoader
 
 logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
@@ -24,8 +24,8 @@ class Converter():
                      draw_transparent, pre_encode, arguments)
         self.output_dir = output_dir
         self.draw_transparent = draw_transparent
-        self.writer_pre_encode = pre_encode
-        self.scale = arguments.output_scale / 100
+        self.pre_encode = pre_encode
+        self.scale = arguments.output_scale / 100.
         self.args = arguments
         self.adjustments = dict(box=None, mask=None, color=None, seamless=None, scaling=None)
         self.load_plugins(output_size, output_has_mask)
@@ -43,11 +43,11 @@ class Converter():
             output_size,
             output_has_mask)
 
-        if self.args.color_adjustment != "none" and self.args.color_adjustment is not None:
+        if self.args.color_adjustment != "none" and self.args.color_adjustment:
             self.adjustments["color"] = PluginLoader.get_converter("color",
                                                                    self.args.color_adjustment)()
 
-        if self.args.scaling != "none" and self.args.scaling is not None:
+        if self.args.scaling != "none" and self.args.scaling:
             self.adjustments["scaling"] = PluginLoader.get_converter("scaling",
                                                                      self.args.scaling)()
         logger.debug("Loaded plugins: %s", self.adjustments)
@@ -89,23 +89,21 @@ class Converter():
         new_image = self.get_new_image(predicted, frame_size)
         patched_face = self.post_warp_adjustments(predicted, new_image)
         patched_face = self.scale_image(patched_face)
-        patched_face = np.rint(patched_face * 255.0).astype("uint8")
-        if self.writer_pre_encode is not None:
-            patched_face = self.writer_pre_encode(patched_face)
+        patched_face = np.rint(patched_face).astype("uint8")
+        retval = self.pre_encode(patched_face) if self.pre_encode else patched_face
         logger.trace("Patched image: '%s'", predicted["filename"])
-        return patched_face
+        return retval
 
     def get_new_image(self, predicted, frame_size):
         """ Get the new face from the predictor and apply box manipulations """
         logger.trace("Getting: (filename: '%s', faces: %s)",
                      predicted["filename"], len(predicted["swapped_faces"]))
-
-        placeholder = predicted["image"] / 255.0
-        placeholder = np.concatenate((placeholder,
-                                      np.zeros((frame_size[1], frame_size[0], 1))),
-                                     axis=-1).astype("float32")
-        for new_face, detected_face in zip(predicted["swapped_faces"],
-                                           predicted["detected_faces"]):
+        print(placeholder.dtype)
+        placeholder = (predicted["image"] / 255.).astype("float32")
+        zeros = np.zeros((frame_size[1], frame_size[0], 1), dtype="float32")
+        placeholder = np.concatenate((placeholder, zeros), axis=-1)
+        dual_generator = zip(predicted["swapped_faces"], predicted["detected_faces"])
+        for new_face, detected_face in dual_generator:
             predicted_mask = new_face[:, :, -1] if new_face.shape[2] == 4 else None
             new_face = new_face[:, :, :3]
             src_face = detected_face.reference_face
@@ -132,12 +130,12 @@ class Converter():
         """ Run the pre-warp adjustments """
         logger.trace("old_face shape: %s, new_face shape: %s, predicted_mask shape: %s",
                      old_face.shape, new_face.shape,
-                     predicted_mask.shape if predicted_mask is not None else None)
+                     predicted_mask.shape if predicted_mask else None)
         new_face = self.adjustments["box"].run(new_face)
         new_face, raw_mask = self.get_image_mask(new_face, detected_face, predicted_mask)
-        if self.adjustments["color"] is not None:
+        if self.adjustments["color"]:
             new_face = self.adjustments["color"].run(old_face, new_face, raw_mask)
-        if self.adjustments["seamless"] is not None:
+        if self.adjustments["seamless"]:
             new_face = self.adjustments["seamless"].run(old_face, new_face, raw_mask)
         logger.trace("returning: new_face shape %s", new_face.shape)
         return new_face
@@ -148,55 +146,48 @@ class Converter():
         mask, raw_mask = self.adjustments["mask"].run(detected_face, predicted_mask)
         if new_face.shape[2] == 4:
             logger.trace("Combining mask with alpha channel box mask")
-            new_face[:, :, -1] = np.minimum(new_face[:, :, -1], mask.squeeze())
+            new_face[:, :, -1] = np.minimum(new_face[:, :, -1:], mask)
         else:
             logger.trace("Adding mask to alpha channel")
-            new_face = np.concatenate((new_face, mask), -1)
-        new_face = np.clip(new_face, 0.0, 1.0)
+            new_face = np.concatenate((new_face, mask), axis=-1)
+        new_face = np.clip(new_face, 0., 1.)
         logger.trace("Got mask. Image shape: %s", new_face.shape)
         return new_face, raw_mask
 
     def post_warp_adjustments(self, predicted, new_image):
         """ Apply fixes to the image after warping """
-        if self.adjustments["scaling"] is not None:
+        if self.adjustments["scaling"]:
             new_image = self.adjustments["scaling"].run(new_image)
-
-        mask = np.repeat(new_image[:, :, -1][:, :, np.newaxis], 3, axis=-1)
-        foreground = new_image[:, :, :3]
-        background = (predicted["image"][:, :, :3] / 255.0) * (1.0 - mask)
-
-        foreground *= mask
-        frame = foreground + background
+        mask = new_image[:, :, -1:]
+        foreground = new_image[:, :, :3] * 255.
+        background = predicted["image"][:, :, :3]
+        frame = foreground * mask + background * (1. - mask)
         frame = self.add_alpha_mask(frame, predicted)
-
-        np.clip(frame, 0.0, 1.0, out=frame)
+        frame = np.clip(frame, 0., 255.)
         return frame
 
     def add_alpha_mask(self, frame, predicted):
         """ Adding a 4th channel should happen after all other channel operations
             Add the default mask as 4th channel for saving as png with alpha channel """
-        if not self.draw_transparent:
-            return frame
-        logger.trace("Creating transparent image: '%s'", predicted["filename"])
-        mask_type = getattr(model_masks, model_masks.get_default_mask())
-        final_mask = np.zeros(frame.shape[:2] + (1, ), dtype="float32")
-
-        for detected_face in predicted["detected_faces"]:
-            landmarks = detected_face.landmarks_as_xy
-            final_mask = cv2.bitwise_or(final_mask,  # pylint: disable=no-member
-                                        mask_type(landmarks, frame, channels=1).mask)
-        frame = np.concatenate((frame, np.expand_dims(final_mask, axis=-1)), axis=-1)
-        logger.trace("Created transparent image: '%s'", predicted["filename"])
+        if self.draw_transparent:
+            logger.trace("Creating transparent image: '%s'", predicted["filename"])
+            mask_type = get_default_mask()
+            final_mask = np.zeros(frame.shape[:2] + (1, ), dtype="float32")
+            for detected_face in predicted["detected_faces"]:
+                landmarks = detected_face.landmarks_as_xy
+                mask = mask_class(landmarks, frame, mask_type, channels=1).mask
+                final_mask = np.bitwise_or(final_mask, mask)
+            frame = np.concatenate((frame, np.expand_dims(final_mask, axis=-1)), axis=-1)
+            logger.trace("Created transparent image: '%s'", predicted["filename"])
         return frame
 
     def scale_image(self, frame):
         """ Scale the image if requested """
-        if self.scale == 1:
-            return frame
-        logger.trace("source frame: %s", frame.shape)
-        interp = cv2.INTER_CUBIC if self.scale > 1 else cv2.INTER_AREA  # pylint: disable=no-member
-        dims = (round((frame.shape[1] / 2 * self.scale) * 2),
-                round((frame.shape[0] / 2 * self.scale) * 2))
-        frame = cv2.resize(frame, dims, interpolation=interp)  # pylint: disable=no-member
-        logger.trace("resized frame: %s", frame.shape)
+        if self.scale != 1:
+            logger.trace("source frame: %s", frame.shape)
+            fx = frame.shape[1] * self.scale
+            fy = frame.shape[0] * self.scale
+            interp = cv2.INTER_CUBIC if self.scale > 1 else cv2.INTER_AREA  # pylint: disable=no-member
+            frame = cv2.resize(frame, fx=fx, fy=fy, interpolation=interp)  # pylint: disable=no-member
+            logger.trace("resized frame: %s", frame.shape)
         return frame
