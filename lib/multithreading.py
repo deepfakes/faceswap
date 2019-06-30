725a5c1d6b1aa5a7cb778cdd2b08b6fed47d477c
torzdf <36920800+torzdf@users.noreply.github.com>
2019-06-30 10:58:38 +0100

    FixedProducerDispatcher: Move args + kwargs to trace log

--- lib/multithreading.py
+++ lib/multithreading.py
@@ -112,9 +112,10 @@ class FixedProducerDispatcher():
 
     def __init__(self, method, shapes, in_queue, out_queue,
                  args=tuple(), kwargs={}, ctype=c_float, workers=1, buffers=None):
-        logger.debug("Initializing %s: (method: '%s', shapes: %s, args: %s, kwargs: %s, "
-                     "ctype: %s, workers: %s, buffers: %s)", self.__class__.__name__, method,
-                     shapes, args, kwargs, ctype, workers, buffers)
+        logger.debug("Initializing %s: (method: '%s', shapes: %s, ctype: %s, workers: %s, "
+                     "buffers: %s)", self.__class__.__name__, method, shapes, ctype, workers,
+                     buffers)
+        logger.trace("args: %s, kwargs: %s", args, kwargs)
         if buffers is None:
             buffers = workers * 2
         else:
