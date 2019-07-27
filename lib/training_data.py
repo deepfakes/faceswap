f07095b2dba35b22a54e0d7c5e66a944fd69315c
Kyle <vrooman.kyle@gmail.com>
2019-07-27 02:08:29 -0500

    Update training_data.py
    white space

--- lib/training_data.py
+++ lib/training_data.py
@@ -377,7 +377,7 @@ class ImageManipulation():
                    "from the Extract process.".format(width, height))
             raise FaceswapError(msg) from err
 
-        range_ = np.linspace(height // 2 - coverage , height // 2 + coverage, 5, dtype='float32')
+        range_ = np.linspace(height // 2 - coverage, height // 2 + coverage, 5, dtype='float32')
         mapx = np.broadcast_to(range_, (5, 5)).copy()
         mapy = mapx.T
         # mapx, mapy = np.float32(np.meshgrid(range_,range_)) # instead of broadcast
