c6319ca4f9bd8bf87aa5df17a31675e4c471735b
Artem Ivanov <37909402+andenixa@users.noreply.github.com>
2019-07-30 01:30:13 +0300

    Model output size assertions fix (#816)
    * added sanity check for RealFace output size to be divisible by 32

--- plugins/train/model/realface.py
+++ plugins/train/model/realface.py
@@ -51,9 +51,9 @@ class Model(ModelBase):
             logger.error("Config error: input_size must be between 64 and 128 and be divisible by "
                          "16.")
             exit(1)
-        if not 64 <= self.config["output_size"] <= 256 or self.config["output_size"] % 16 != 0:
+        if not 64 <= self.config["output_size"] <= 256 or self.config["output_size"] % 32 != 0:
             logger.error("Config error: output_size must be between 64 and 256 and be divisible "
-                         "by 16.")
+                         "by 32.")
             exit(1)
         logger.debug("Input and output sizes are valid")
 
