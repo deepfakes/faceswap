8e9035b8ed2f0655617bca93dd497c32e38e5cc0
torzdf <36920800+torzdf@users.noreply.github.com>
2019-08-02 09:28:49 +0000

    Sort tool. Correctly set TF Verbosity for sort by face

--- tools/sort.py
+++ tools/sort.py
@@ -57,7 +57,7 @@ class Sort():
 
         # Load VGG Face if sorting by face
         if self.args.sort_method.lower() == "face":
-            self.vgg_face = VGGFace(backend=self.args.backend)
+            self.vgg_face = VGGFace(backend=self.args.backend, loglevel=self.args.loglevel)
 
         # If logging is enabled, prepare container
         if self.args.log_changes:
