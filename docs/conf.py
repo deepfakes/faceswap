d691612a89ab855958f54eeb25035a3bb765cf1c
torzdf <36920800+torzdf@users.noreply.github.com>
2019-09-15 15:38:29 +0000

    pep8 fix

--- docs/conf.py
+++ docs/conf.py
@@ -30,8 +30,7 @@ release = '0.99'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.napoleon',
-]
+extensions = ['sphinx.ext.napoleon', ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
