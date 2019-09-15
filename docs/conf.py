cd7d74e2798c560b7fc7ee560c5d5453c6bc2d42
torzdf <36920800+torzdf@users.noreply.github.com>
2019-09-15 16:15:38 +0000

    Update Sphinx conf

--- docs/conf.py
+++ docs/conf.py
@@ -52,3 +52,5 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+master_doc = 'index'
