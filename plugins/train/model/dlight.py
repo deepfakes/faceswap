257887a56910c47999776f841743fea3d7dbf100
AnDenixa <ivanovar-08@mail.ru>
2019-10-26 02:22:14 +0300

    ops, forgot fast-decoder fixed the remaining n docstrings

--- plugins/train/model/dlight.py
+++ plugins/train/model/dlight.py
@@ -12,20 +12,20 @@ import sys
 import types
 
 from keras.initializers import RandomNormal
-from keras.layers import Dense, Flatten, Input, Reshape, AveragePooling2D, LeakyReLU
+from keras.layers import Add, Dense, Flatten, Input, Reshape, AveragePooling2D, LeakyReLU
 from keras.layers.convolutional import UpSampling2D, Conv2DTranspose
+from keras.layers.core import Dropout
 from keras.layers.merge import Concatenate
+from keras.layers.normalization import BatchNormalization
 from keras.models import Model as KerasModel
+
+from lib.utils import FaceswapError
 
 from ._base import logger
 from .original import Model as OriginalModel
-from lib.utils import FaceswapError
-from keras.layers.normalization import BatchNormalization
-from keras.layers.core import Dropout
+
 
 # [P] TODO Move upscale2x_hyb to nnblocks.py (after testing)
-
-
 # <<< DeLight Model Blocks >>> #
 def upscale2x_hyb(self, inp, filters, kernel_size=3, padding='same',
                   sr_ratio=0.5, scale_factor=2, interpolation='bilinear',
@@ -52,8 +52,27 @@ def upscale2x_hyb(self, inp, filters, kernel_size=3, padding='same',
     return var_x
 
 
+def upscale2x_fast(self, inp, filters, kernel_size=3, padding='same',
+                  sr_ratio=0.5, scale_factor=2, interpolation='bilinear',
+                  res_block_follows=False, **kwargs):
+    """Fast Upscale Layer"""
+    name = self.get_name("upscale2x_fast")
+    var_x = inp
+
+    var_x2 = self.conv2d(var_x, filters,  kernel_size=3, padding=padding,
+                         name="{}_conv2d".format(name), **kwargs)
+    var_x2 = UpSampling2D(size=(scale_factor, scale_factor), interpolation=interpolation,
+                          name="{}_upsampling2D".format(name))(var_x2)    
+                              
+    var_x1 = self.upscale(var_x, filters, kernel_size=kernel_size,
+                          padding=padding, scale_factor=scale_factor,
+                          res_block_follows=res_block_follows, **kwargs)    
+    var_x =  Add()([var_x2, var_x1])
+    return var_x
+
+
 class Model(OriginalModel):
-    """ Improved Autoencoder Model """
+    """ DeLight Autoencoder Model """
 
     def __init__(self, *args, **kwargs):
         logger.debug("Initializing %s: (args: %s, kwargs: %s",
@@ -108,15 +127,18 @@ class Model(OriginalModel):
 
     def build(self):
         self._detail_level_setup()
+        # monkey patch-in nn_blocks
         self.blocks.upscale2x_hyb = types.MethodType(upscale2x_hyb, self.blocks)
-
+        self.blocks.upscale2x_fast = types.MethodType(upscale2x_fast, self.blocks)
         super().build()
 
     def add_networks(self):
-        """ Add the realface model weights """
+        """ Add the DeLight model weights """
         logger.debug("Adding networks")
         self.add_network("decoder", "a", self.decoder_a(), is_output=True)
-        self.add_network("decoder", "b", self.decoder_b(), is_output=True)
+        self.add_network("decoder", "b", 
+                         self.decoder_b() if self.details>0 else self.decoder_b_fast(), 
+                         is_output=True)
         self.add_network("encoder", None, self.encoder())
         logger.debug("Added networks")
 
@@ -213,6 +235,52 @@ class Model(OriginalModel):
 
         return KerasModel([input_], outputs=outputs)
 
+    def decoder_b_fast(self):
+        """ DeLight Fast Decoder B(new face) Network  """
+        input_ = Input(shape=(4, 4, 1024))
+
+        decoder_b_complexity = 512
+        mask_complexity = 128
+
+        var_xy = input_
+
+        var_xy = self.blocks.upscale(var_xy, 512, scale_factor=self.upscale_ratio,
+                                     res_block_follows=True)
+        var_x = var_xy
+
+        var_x = self.blocks.res_block(var_x, 512, use_bias=False)
+        
+        var_x = self.blocks.upscale2x_fast(var_x, decoder_b_complexity, res_block_follows=True)        
+        var_x = self.blocks.res_block(var_x, decoder_b_complexity, use_bias=False)
+        
+        var_x = self.blocks.upscale2x_fast(var_x, decoder_b_complexity // 2, res_block_follows=True)        
+        var_x = self.blocks.res_block(var_x, decoder_b_complexity // 2, use_bias=False)
+                
+        var_x = self.blocks.upscale2x_fast(var_x, decoder_b_complexity // 4, res_block_follows=True)                
+        var_x = self.blocks.res_block(var_x, decoder_b_complexity // 4, use_bias=False)
+        
+        var_x = self.blocks.upscale2x_fast(var_x, decoder_b_complexity//8)        
+
+        var_x = self.blocks.conv2d(var_x, 3, kernel_size=5, padding="same",
+                                   activation="sigmoid", name="face_out")
+
+        outputs = [var_x]
+
+        if self.config.get("mask_type", False):
+            var_y = var_xy  # mask decoder
+
+            var_y = self.blocks.upscale2x_hyb(var_y, mask_complexity)
+            var_y = self.blocks.upscale2x_hyb(var_y, mask_complexity // 2)
+            var_y = self.blocks.upscale2x_hyb(var_y, mask_complexity // 4)
+            var_y = self.blocks.upscale2x_hyb(var_y, mask_complexity // 8)
+
+            var_y = self.blocks.conv2d(var_y, 1, kernel_size=5, padding="same",
+                                       activation="sigmoid", name="mask_out")
+
+            outputs.append(var_y)
+
+        return KerasModel([input_], outputs=outputs)
+    
     def decoder_b(self):
         """ DeLight Decoder B(new face) Network  """
         input_ = Input(shape=(4, 4, 1024))
@@ -258,4 +326,4 @@ class Model(OriginalModel):
 
             outputs.append(var_y)
 
-        return KerasModel([input_], outputs=outputs)
+        return KerasModel([input_], outputs=outputs)    
