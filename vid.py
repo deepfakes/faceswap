b55c87390d32b1d11d2bda001e29f8ba7435839e
torzdf <36920800+torzdf@users.noreply.github.com>
2020-03-04 00:34:51 +0000

    Remove accidently pushed test file

--- vid.py
+++
@@ -1,9 +0,0 @@
-from lib.logger import log_setup
-log_setup("INFO", None, None, False)
-
-from lib.image import ImagesLoader
-
-a = ImagesLoader("/home/matt/fake/test/extract/match.mp4")
-
-b = a.frame_from_index(10)
-print([c for c in b])
