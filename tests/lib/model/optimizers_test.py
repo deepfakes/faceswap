3602fc576e530b155e4b5883dc69584a156d594c
torzdf <36920800+torzdf@users.noreply.github.com>
2020-08-10 17:38:19 +0000

    Relax optimizer test threshold

--- tests/lib/model/optimizers_test.py
+++ tests/lib/model/optimizers_test.py
@@ -74,5 +74,5 @@ def _test_optimizer(optimizer, target=0.75):
 @pytest.mark.parametrize("dummy", [None], ids=[get_backend().upper()])
 def test_adam(dummy):  # pylint:disable=unused-argument
     """ Test for custom Adam optimizer """
-    _test_optimizer(k_optimizers.Adam())
-    _test_optimizer(k_optimizers.Adam(decay=1e-3))
+    _test_optimizer(k_optimizers.Adam(), target=0.6)
+    _test_optimizer(k_optimizers.Adam(decay=1e-3), target=0.6)
