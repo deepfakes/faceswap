abfaa69f2e5d3ad83af42ae585bbe99991de0505
torzdf <36920800+torzdf@users.noreply.github.com>
2020-09-19 14:58:52 +0100

    Vectorize smoothing

--- lib/gui/stats.py
+++ lib/gui/stats.py
@@ -17,6 +17,8 @@ from lib.serializer import get_serializer
 
 logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 
+# TODO Timestamps float64, loss float32/float16
+
 
 def convert_time(timestamp):
     """ Convert time stamp to total hours, minutes and seconds.
@@ -595,7 +597,7 @@ class Calculations():
         logger.debug("Flattening outliers: %s", data.shape)
         mean = np.mean(data)
         limit = np.std(data)
-        logger.info("mean: %s, limit: %s", mean, limit)
+        logger.debug("mean: %s, limit: %s", mean, limit)
         retdata = np.where(abs(data - mean) < limit, data, mean)
         logger.debug("Flattened outliers")
         return retdata
@@ -675,17 +677,17 @@ class Calculations():
         """
         logger.debug("Calculating Average")
         window = self._args["avg_samples"]
-        presample = ceil(window / 2)
+        pad = ceil(window / 2)
         datapoints = len(data)
+
         if datapoints <= (self._args["avg_samples"] * 2):
             logger.info("Not enough data to compile rolling average")
             return np.array([], dtype="float64")
+
         avgs = np.cumsum(data, dtype="float64")
         avgs[window:] = avgs[window:] - avgs[:-window]
         avgs = avgs[window - 1:] / window
-        avgs = np.pad(avgs,
-                      (presample, datapoints - (avgs.shape[0] + presample)),
-                      constant_values=(np.nan,))
+        avgs = np.pad(avgs, (pad, datapoints - (avgs.shape[0] + pad)), constant_values=(np.nan,))
         logger.debug("Calculated Average: shape: %s", avgs.shape)
         return avgs
 
@@ -699,18 +701,10 @@ class Calculations():
 
         Returns
         -------
-        list
+        :class:`numpy.ndarray`
             The smoothed data
         """
-        last = data[0]  # First value in the plot (first time step)
-        weight = self._args["smooth_amount"]
-        smoothed = list()
-        for point in data:
-            smoothed_val = last * weight + (1 - weight) * point  # Calculate smoothed value
-            smoothed.append(smoothed_val)                        # Save it
-            last = smoothed_val                                  # Anchor the last smoothed value
-
-        return smoothed
+        return ExponentialMovingAverage(data, self._args["smooth_amount"])()
 
     @classmethod
     def _calc_trend(cls, data):
@@ -737,3 +731,158 @@ class Calculations():
         trend = poly(x_range)
         logger.debug("Calculated Trend")
         return trend
+
+
+class ExponentialMovingAverage():  # pylint:disable=too-few-public-methods
+    """ Reshapes data before calculating exponential moving average, then iterates once over the
+    rows to calculate the offset without precision issues.
+
+    Parameters
+    ----------
+    data: :class:`numpy.ndarray`
+        A 1 dimensional numpy array to obtain smoothed data for
+    amount: float
+        in the range (0.0, 1.0) The alpha parameter (smoothing amount) for the moving average.
+
+    Notes
+    -----
+    Adapted from: https://stackoverflow.com/questions/42869495
+    """
+    def __init__(self, data, amount):
+        assert data.ndim == 1
+        amount = min(max(amount, 0.001), 0.999)
+
+        self._data = data
+        self._alpha = 1. - amount
+        self._dtype = "float32" if data.dtype == np.float32 else "float64"
+        self._row_size = self._get_max_row_size()
+        self._out = np.empty_like(data, dtype=self._dtype)
+
+    def __call__(self):
+        """ Perform the exponential moving average calculation.
+
+        Returns
+        -------
+        :class:`numpy.ndarray`
+            The smoothed data
+        """
+        if self._data.size <= self._row_size:
+            self._ewma_vectorized(self._data, self._out)  # Normal function can handle this input
+        else:
+            self._ewma_vectorized_safe()  # Use the safe version
+        return self._out
+
+    def _get_max_row_size(self):
+        """ Returns the maximum row size possible on the running platform for the given dtype.
+
+        Notes
+        -----
+        Might not be the optimal value for speed, which is hard to predict due to numpy
+        optimizations.
+        """
+        # Use :func:`np.finfo(dtype).eps` if you are worried about accuracy and want to be safe.
+        epsilon = np.finfo(self._dtype).tiny
+        # If this produces an OverflowError, make epsilon larger:
+        retval = int(np.log(epsilon) / np.log(1 - self._alpha)) + 1
+        logger.debug("row_size: %s", retval)
+        return retval
+
+    def _ewma_vectorized_safe(self):
+        """ Perform the vectorized exponential moving average by the most appropriate method
+        """
+        num_rows = int(self._data.size // self._row_size)  # the number of rows to use
+        leftover = int(self._data.size % self._row_size)  # the amount of data leftover
+        first_offset = self._data[0]
+
+        if leftover > 0:
+            # set temporary results to slice view of out parameter
+            out_main_view = np.reshape(self._out[:-leftover], (num_rows, self._row_size))
+            data_main_view = np.reshape(self._data[:-leftover], (num_rows, self._row_size))
+        else:
+            out_main_view = self._out.reshape(-1, self._row_size)
+            data_main_view = self._data.reshape(-1, self._row_size)
+
+        self._ewma_vectorized_2d(data_main_view, out_main_view)  # get the scaled cumulative sums
+
+        scaling_factors = (1 - self._alpha) ** np.arange(1, self._row_size + 1)
+        last_scaling_factor = scaling_factors[-1]
+
+        # create offset array
+        offsets = np.empty(out_main_view.shape[0], dtype=self._dtype)
+        offsets[0] = first_offset
+        # iteratively calculate offset for each row
+
+        for i in range(1, out_main_view.shape[0]):
+            offsets[i] = offsets[i - 1] * last_scaling_factor + out_main_view[i - 1, -1]
+
+        # add the offsets to the result
+        out_main_view += offsets[:, np.newaxis] * scaling_factors[np.newaxis, :]
+
+        if leftover > 0:
+            # process trailing data in the 2nd slice of the out parameter
+            self._ewma_vectorized(self._data[-leftover:],
+                                  self._out[-leftover:],
+                                  offset=out_main_view[-1, -1])
+
+    def _ewma_vectorized(self, data, out, offset=None):
+        """ Calculates the exponential moving average over a vector. Will fail for large inputs.
+
+        The result is processed in place into the array passed to the `out` parameter
+
+        Parameters
+        ----------
+        data: :class:`numpy.ndarray`
+            A 1 dimensional numpy array to obtain smoothed data for
+        out: :class:`numpy.ndarray`
+            A location into which the result is stored. It must have the same shape and dtype as
+            the input data
+        :param offset: optional
+            The offset for the moving average, scalar. Defaults to data[0].
+        """
+        if data.size < 1:  # empty input, return empty array
+            return
+
+        offset = data[0] if offset is None else offset
+
+        # scaling_factors -> 0 as len(data) gets large. This leads to divide-by-zeros below
+        scaling_factors = np.power(1. - self._alpha, np.arange(data.size + 1, dtype=self._dtype),
+                                   dtype=self._dtype)
+        # create cumulative sum array
+        np.multiply(data, (self._alpha * scaling_factors[-2]) / scaling_factors[:-1],
+                    dtype=self._dtype, out=out)
+        np.cumsum(out, dtype=self._dtype, out=out)
+
+        out /= scaling_factors[-2::-1]  # cumulative sums / scaling
+
+        if offset != 0:
+            offset = np.array(offset, copy=False).astype(self._dtype, copy=False)
+            out += offset * scaling_factors[1:]
+
+    def _ewma_vectorized_2d(self, data, out):
+        """ Calculates the exponential moving average over the last axis.
+
+        The result is processed in place into the array passed to the `out` parameter
+
+        Parameters
+        ----------
+        data: :class:`numpy.ndarray`
+            A 1 or 2 dimensional numpy array to obtain smoothed data for.
+        out: :class:`numpy.ndarray`
+            A location into which the result is stored. It must have the same shape and dtype as
+            the input data
+        """
+        if data.size < 1:  # empty input, return empty array
+            return
+
+        # calculate the moving average
+        scaling_factors = np.power(1. - self._alpha, np.arange(data.shape[1] + 1,
+                                                               dtype=self._dtype),
+                                   dtype=self._dtype)
+        # create a scaled cumulative sum array
+        np.multiply(data,
+                    np.multiply(self._alpha * scaling_factors[-2],
+                                np.ones((data.shape[0], 1), dtype=self._dtype),
+                                dtype=self._dtype) / scaling_factors[np.newaxis, :-1],
+                    dtype=self._dtype, out=out)
+        np.cumsum(out, axis=1, dtype=self._dtype, out=out)
+        out /= scaling_factors[np.newaxis, -2::-1]
