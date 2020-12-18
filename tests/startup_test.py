f234757542c979e5de23c586db9d33709cf0db09
torzdf <36920800+torzdf@users.noreply.github.com>
2020-12-18 18:53:55 +0000

    TypoFix

--- tests/startup_test.py
+++ tests/startup_test.py
@@ -26,4 +26,4 @@ def test_keras(dummy):  # pylint:disable=unused-argument
     """ Sanity check to ensure that tensorflow keras is being used for CPU and standard
     keras for AMD. """
     assert ((_BACKEND == "cpu" and keras.__version__ == "2.4.0") or
-            (_BACKEND == "amd" and not keras.__version__ == "2.2.4"))
+            (_BACKEND == "amd" and keras.__version__ == "2.2.4"))
