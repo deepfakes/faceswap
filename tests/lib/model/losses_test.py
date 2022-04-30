9b117018ea3b4784b206b4c22ce874f206a2048d
torzdf <36920800+torzdf@users.noreply.github.com>
2022-04-30 18:55:45 +0100

    update tests

--- tests/lib/model/losses_test.py
+++ tests/lib/model/losses_test.py
@@ -77,7 +77,7 @@ def test_dssim_channels_last(dummy):  # pylint:disable=unused-argument
     """ Basic test for DSSIM Loss """
     prev_data = K.image_data_format()
     K.set_image_data_format('channels_last')
-    for input_dim, kernel_size in zip([32, 33], [2, 3]):
+    for input_dim in (32, 33):
         input_shape = [input_dim, input_dim, 3]
         var_x = np.random.random_sample(4 * input_dim * input_dim * 3)
         var_x = var_x.reshape([4] + input_shape)
@@ -90,7 +90,7 @@ def test_dssim_channels_last(dummy):  # pylint:disable=unused-argument
         model.add(Conv2D(3, (3, 3), padding='same', input_shape=input_shape,
                          activation='relu'))
         adam = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-8)
-        model.compile(loss=losses.DSSIMObjective(kernel_size=kernel_size),
+        model.compile(loss=losses.DSSIMObjective(),
                       metrics=['mse'],
                       optimizer=adam)
         model.fit(var_x, var_y, batch_size=2, epochs=1, shuffle='batch')
@@ -98,13 +98,13 @@ def test_dssim_channels_last(dummy):  # pylint:disable=unused-argument
         # Test same
         x_1 = K.constant(var_x, 'float32')
         x_2 = K.constant(var_x, 'float32')
-        dssim = losses.DSSIMObjective(kernel_size=kernel_size)
+        dssim = losses.DSSIMObjective()
         assert_allclose(0.0, K.eval(dssim(x_1, x_2)), atol=1e-4)
 
         # Test opposite
         x_1 = K.zeros([4] + input_shape)
         x_2 = K.ones([4] + input_shape)
-        dssim = losses.DSSIMObjective(kernel_size=kernel_size)
+        dssim = losses.DSSIMObjective()
         assert_allclose(0.5, K.eval(dssim(x_1, x_2)), atol=1e-4)
 
     K.set_image_data_format(prev_data)
