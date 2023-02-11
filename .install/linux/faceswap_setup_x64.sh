757088f68b6d2e86f626dbf3fb0f0e1a8e04e787
torzdf <36920800+torzdf@users.noreply.github.com>
2023-02-11 23:58:45 +0000

    bugfix: Linux installer -cpu mode

--- .install/linux/faceswap_setup_x64.sh
+++ .install/linux/faceswap_setup_x64.sh
@@ -411,8 +411,7 @@ clone_faceswap() {
 setup_faceswap() {
     # Run faceswap setup script
     info "Setting up Faceswap..."
-    if [ $VERSION != "cpu" ] ; then args="--$VERSION" ; else args="" ; fi
-    python -u "$DIR_FACESWAP/setup.py" --installer $args
+    python -u "$DIR_FACESWAP/setup.py" --installer --$VERSION
 }
 
 create_gui_launcher () {
