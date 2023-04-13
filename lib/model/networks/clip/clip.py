c31469500c280c7e72b71bd2f460c1036de42af2
Bryan Lyon <bryanlyon@users.noreply.github.com>
2023-04-13 14:51:03 -0700

    Added some docstrings

--- lib/model/networks/clip/clip.py
+++ lib/model/networks/clip/clip.py
@@ -1,4 +1,4 @@
-#Clip implmented in TF2 from https://github.com/RobertBiehl/CLIP-tf2
+# Clip implmented in TF2 from https://github.com/RobertBiehl/CLIP-tf2
 
 from collections import OrderedDict
 from typing import Tuple, Union
@@ -9,12 +9,39 @@ import tensorflow as tf
 from .resnet import ModifiedResNet
 from .transformer import Transformer
 from .visual_transformer import VisualTransformer
-from tensorflow.keras.layers import LayerNormalization
 
 
 class CLIP(keras.Model):
+    """
+    A Convolutional Language-Image Pre-Training (CLIP) model that encodes images and text into a shared latent space.
+    Args:
+        embed_dim (int): Dimensionality of the final shared embedding space.
+        image_resolution (int): Spatial resolution of the input images.
+        vision_layers (Union[Tuple[int, int, int, int], int]): Number of layers in the visual encoder, or a tuple of
+            layer configurations for a custom ResNet visual encoder.
+        vision_width (int): Width of the visual encoder layers.
+        vision_patch_size (int): Size of the patches to be extracted from the images.
+        context_length (int): Length of the input text sequences.
+        vocab_size (int): Size of the vocabulary.
+        transformer_width (int): Width of the transformer layers.
+        transformer_heads (int): Number of heads in the transformer attention mechanism.
+        transformer_layers (int): Number of transformer layers.
+
+    Attributes:
+        image_resolution (int): Spatial resolution of the input images.
+        context_length (int): Length of the input text sequences.
+        visual (keras.Model): Visual encoder module.
+        transformer (Transformer): Transformer module for the text encoder.
+        vocab_size (int): Size of the vocabulary.
+        token_embedding (tf.Variable): Embedding layer for the text encoder.
+        positional_embedding (tf.Variable): Positional encoding layer for the text encoder.
+        ln_final (keras.layers.LayerNormalization): Layer normalization for the final transformer output.
+        text_projection (tf.Variable): Projection layer for the final text embedding.
+        logit_scale (tf.Variable): Scaling factor for the final cosine similarity scores.
+
+    """
     def __init__(self,
-                 embed_dim: int,
+                 embed_dim: inzt,
                  # vision
                  image_resolution: int,
                  vision_layers: Union[Tuple[int, int, int, int], int],
@@ -27,6 +54,25 @@ class CLIP(keras.Model):
                  transformer_heads: int,
                  transformer_layers: int
                  ):
+        """
+        Initializes the CLIP model.
+
+        Args:
+            embed_dim (int): Dimensionality of the final shared embedding space.
+            image_resolution (int): Spatial resolution of the input images.
+            vision_layers (Union[Tuple[int, int, int, int], int]): Number of layers in the visual 
+                encoder, or a tuple of layer configurations for a custom ResNet visual encoder.
+            vision_width (int): Width of the visual encoder layers.
+            vision_patch_size (int): Size of the patches to be extracted from the images.
+            context_length (int): Length of the input text sequences.
+            vocab_size (int): Size of the vocabulary.
+            transformer_width (int): Width of the transformer layers.
+            transformer_heads (int): Number of heads in the transformer attention mechanism.
+            transformer_layers (int): Number of transformer layers.
+
+        Returns:
+            None
+        """
         super().__init__()
 
         self.image_resolution = image_resolution
@@ -63,26 +109,35 @@ class CLIP(keras.Model):
         )
 
         self.vocab_size = vocab_size
-        self.token_embedding = tf.Variable(tf.zeros((vocab_size, transformer_width)), name="token_embedding")
-        self.positional_embedding = tf.Variable(tf.zeros((self.context_length, transformer_width)), name="positional_embedding")
-        self.ln_final = keras.layers.LayerNormalization(name="ln_final")
+        self.token_embedding = tf.Variable(
+            tf.zeros((vocab_size, transformer_width)), name="token_embedding")
+        self.positional_embedding = tf.Variable(
+            tf.zeros((self.context_length, transformer_width)), name="positional_embedding")
+        self.ln_final = keras.layers.LayerNormalization(epsilon=1e-05, name="ln_final")
 
-        self.text_projection = tf.Variable(tf.zeros((transformer_width, embed_dim)), name="text_projection")
-        self.logit_scale = tf.Variable(np.ones([]) * np.log(1 / 0.07), dtype=tf.float32, name="logit_scale")
+        self.text_projection = tf.Variable(
+            tf.zeros((transformer_width, embed_dim)), name="text_projection")
+        self.logit_scale = tf.Variable(np.ones([]) * np.log(1 / 0.07),
+                                       dtype=tf.float32, name="logit_scale")
 
     def initialize_parameters(self):
         # TODO: convert to tf, for model initialization (not needed for pretrained weights)
         self.token_embedding.assign(tf.random.normal(self.token_embedding.shape, stddev=0.02))
-        self.positional_embedding.assign(tf.random.normal(self.positional_embedding.shape, stddev=0.01))
+        self.positional_embedding.assign(tf.random.normal(
+            self.positional_embedding.shape, stddev=0.01))
 
         from resnet import ModifiedResNet
         if isinstance(self.visual, ModifiedResNet):
             if self.visual.attnpool is not None:
                 std = self.visual.attnpool.c_proj.in_features ** -0.5
-                self.visual.attnpool.q_proj.weight.assign(tf.random.normal(self.visual.attnpool.q_proj.weight.shape, stddev=std))
-                self.visual.attnpool.k_proj.weight.assign(tf.random.normal(self.visual.attnpool.k_proj.weight.shape, stddev=std))
-                self.visual.attnpool.v_proj.weight.assign(tf.random.normal(self.visual.attnpool.v_proj.weight.shape, stddev=std))
-                self.visual.attnpool.c_proj.weight.assign(tf.random.normal(self.visual.attnpool.c_proj.weight.shape, stddev=std))
+                self.visual.attnpool.q_proj.weight.assign(tf.random.normal(
+                    self.visual.attnpool.q_proj.weight.shape, stddev=std))
+                self.visual.attnpool.k_proj.weight.assign(tf.random.normal(
+                    self.visual.attnpool.k_proj.weight.shape, stddev=std))
+                self.visual.attnpool.v_proj.weight.assign(tf.random.normal(
+                    self.visual.attnpool.v_proj.weight.shape, stddev=std))
+                self.visual.attnpool.c_proj.weight.assign(tf.random.normal(
+                    self.visual.attnpool.c_proj.weight.shape, stddev=std))
 
             for resnet_block in [self.visual.layer1, self.visual.layer2, self.visual.layer3, self.visual.layer4]:
                 for name, param in resnet_block.named_parameters():
@@ -115,8 +170,8 @@ class CLIP(keras.Model):
 
         i = tf.range(n_dest)[:, None]
         j = tf.range(n_src)
-        m = i >= j - n_src + n_dest
-        mask = tf.cast(m, dtype)
+        mask = i >= j - n_src + n_dest
+        mask = tf.cast(mask, dtype)
         mask = tf.reshape(mask, [1, n_dest, n_src])
         mult = tf.concat(
             [tf.expand_dims(batch_size, -1), tf.constant([1, 1], dtype=tf.int32)], 0
@@ -132,22 +187,33 @@ class CLIP(keras.Model):
         return self.visual(image)
 
     @tf.function(input_signature=[tf.TensorSpec(shape=(None, None), dtype=tf.int32, name="text")])
-    def encode_text(self, text: tf.Tensor):
-        x = tf.nn.embedding_lookup(self.token_embedding, text)
-        x = x + self.positional_embedding
-        x = self.transformer(x)
-        x = self.ln_final(x)
+    def encode_text(self, text: tf.Tensor) -> tf.Tensor:
+        """
+        Encodes the input text using the pretrained model.
+
+        Args:
+            text: A tensor of shape [batch_size, n_ctx], containing the tokenized text.
+
+        Returns:
+            A tensor of shape [batch_size, embed_size], containing the encoded representation
+            of the input text.
+        """
+        var_x = tf.nn.embedding_lookup(self.token_embedding, text)
+        var_x = var_x + self.positional_embedding
+        var_x = self.transformer(var_x)
+        var_x = self.ln_final(var_x)
 
         # x.shape = [batch_size, n_ctx, transformer.width]
-        x_shape = tf.shape(x)
+        x_shape = tf.shape(var_x)
         # take features from the eot embedding (eot_token is the highest number in each sequence)
         eot_token = tf.argmax(text, axis=-1)
 
         # TODO check if dtype is correct
-        idx = tf.transpose(tf.stack((tf.range(0, x_shape[0], dtype=tf.int64), eot_token), axis=0, name='take_features_idx'))
-        x = tf.gather_nd(x, idx) @ self.text_projection
+        idx = tf.transpose(
+            tf.stack((tf.range(0, x_shape[0], dtype=tf.int64), eot_token), axis=0, name='take_features_idx'))
+        var_x = tf.gather_nd(var_x, idx) @ self.text_projection
 
-        return x
+        return var_x
 
     @tf.function(input_signature=[(
         tf.TensorSpec(shape=(None, None, None, 3), dtype=tf.float32, name="image"),
@@ -157,7 +223,8 @@ class CLIP(keras.Model):
         image, text = input
         image_features = self.encode_image(image)
 
-        text = tf.squeeze(text, axis=0) # TODO: find another way to feed data, but keras requires that all input tensors have to have the same batch size
+        # TODO: find another way to feed data, but keras requires that all input tensors have to have the same batch size
+        text = tf.squeeze(text, axis=0)
         text_features = self.encode_text(text)
 
         # normalized features
