0065be30d264475c666d7bff24c1ef968b25628d
torzdf <36920800+torzdf@users.noreply.github.com>
2023-06-25 13:56:54 +0100

    train: re-enable optimizer saving

--- plugins/train/model/_base/model.py
+++ plugins/train/model/_base/model.py
@@ -94,14 +94,7 @@ class ModelBase():
                                 "use. Please select a mask or disable 'Learn Mask'.")
 
         self._mixed_precision = self.config["mixed_precision"]
-        # self._io = IO(self, model_dir, self._is_predict, self.config["save_optimizer"])
-        # TODO - Re-enable saving of optimizer once this bug is fixed:
-        # File "h5py/_objects.pyx", line 54, in h5py._objects.with_phil.wrapper
-        # File "h5py/_objects.pyx", line 55, in h5py._objects.with_phil.wrapper
-        # File "h5py/h5d.pyx", line 87, in h5py.h5d.create
-        # ValueError: Unable to create dataset (name already exists)
-
-        self._io = IO(self, model_dir, self._is_predict, "never")
+        self._io = IO(self, model_dir, self._is_predict, self.config["save_optimizer"])
         self._check_multiple_models()
 
         self._state = State(model_dir,
