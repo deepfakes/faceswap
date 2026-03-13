bbc4752bcf6ddfd3f14b2b55cc88cc1e2e25e281
torzdf <36920800+torzdf@users.noreply.github.com>
2026-03-13 15:03:15 +0000

    bugfix: More python 3.11 string formatting issues

--- lib/infer/profile.py
+++ lib/infer/profile.py
@@ -427,8 +427,8 @@ class Output:
         self._write(self._header_row)
         self._write(["Batch Size"] + [str(int(b)) for b in self._data.batch_sizes[-1]])
         egs = [f"{e:.1f}" for e in self._data.get_samples(-1) / self._run_time]
-        avg_egs = [f"{(self._data.get_samples_stats("mean", -1) / self._run_time):.1f}"]
-        min_egs = [f"{(self._data.get_samples_stats("min", -1) / self._run_time):.1f}"]
+        avg_egs = [f"{(self._data.get_samples_stats('mean', -1) / self._run_time):.1f}"]
+        min_egs = [f"{(self._data.get_samples_stats('min', -1) / self._run_time):.1f}"]
         self._write(["EG/S"] + egs + avg_egs + min_egs)
 
         if self._data.has_detector and self._data.face_scaling > 1:
