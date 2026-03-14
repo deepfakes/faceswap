2d6710b45436d364e6f9526256aacf0028febbed
torzdf <36920800+torzdf@users.noreply.github.com>
2026-03-14 07:25:06 +0000

    Bugfix: Aligner filters mask shape mismatch

--- lib/infer/align.py
+++ lib/infer/align.py
@@ -965,20 +965,22 @@ class AlignedFilter:  # pylint:disable=too-many-instance-attributes
         batch
             The batch object to perform filtering on with the landmarks populated
         """
-        if not self.enabled:
+        if not self.enabled or batch.landmarks is None:
             return
         if batch.landmark_type not in (LandmarkType.LM_2D_68, LandmarkType.LM_2D_98):
             logger.warning("[Align filter] Filters are not supported for %s landmarks",
                            batch.landmark_type)
             self.enabled = False
             return
-        norm_lms = batch.aligned.landmarks_normalized
         if self._features:
-            self._handle_filtered("features", batch, self._filter_features(norm_lms))
+            self._handle_filtered("features",
+                                  batch,
+                                  self._filter_features(batch.aligned.landmarks_normalized))
         if self._min_scale > 0.0 or self._max_scale > 0.0:
             self._handle_filtered("scale", batch, self._filter_scale(batch))
         if self._distance > 0.0:
-            d_msk = np.abs(norm_lms[:, 17:] - self._mean_face).mean(axis=(1, 2)) <= self._distance
+            d_msk = np.abs(batch.aligned.landmarks_normalized[:, 17:] -
+                           self._mean_face).mean(axis=(1, 2)) <= self._distance
             self._handle_filtered("distance", batch, d_msk)
         if self._roll > 0.0:
             r_msk = np.abs(Batch3D.roll(batch.aligned.rotation)) <= self._roll
